281bf54def50b7bdd3b78dddb84b52d9c1f03f6b
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-25 15:55:14 +0300

    Update init.c

--- Software/lpc1114/init.c
+++ Software/lpc1114/init.c
@@ -4,15 +4,22 @@ void init(void)
 LPC_SYSCON->SYSAHBCLKDIV              = 0x1;                    //set clock divider for core to 1
 LPC_SYSCON->MAINCLKSEL                &= ~(0x03);               //set main clock to IRC oscillator, 
                                                                 //if not system will lock up when PLL turns off
-LPC_SYSCON->MAINCLKUEN                &= ~(1);
-
-
-
-
-
-
-
-
-
+LPC_SYSCON->MAINCLKUEN                &= ~(1);                  //write a zero to the MAINCLKUEN register,
+                                                                //necessary for MAINCLKSEL to update
+LPC_SYSCON->MAINCLKUEN                |= 1;                     //write a one to the MAINCLKUEN register,
+                                                                //necessary for MAINCLKSEL to update
+LPC_SYSCON->SYSPLLCLKSEL              = 0x0;                    //connect system oscillator to SYSTEM PLL 
+LPC_SYSCON->SYSPLLCLKUEN              &= ~(1);                  //write a zero to SYSPLLUEN register,
+                                                                //necessary for SYSPLLCLKSEL to update
+LPC_SYSCON->SYSPLLCLKUEN              |= 1;                     //write a one to SYSPLLUEN register,
+                                                                //necessary for SYSPLLCLKSEL to update
+LPC_SYSCON->PDRUNCFG                  |= (1<<7);                //power down the PLL before changing divider values
+LPC_SYSCON->SYSPLLCTRL                = 0x23;                   //set MSEL = 0x00011 and PSEL = 0x01
+LPC_SYSCON->PDRUNCFG                  &= ~(1<<7);               //power up PLL after divider values changed 
+while((LPC_SYSCON->SYSPLLSTAT & 1) == 0);                       //wait for PLL to lock
+LPC_SYSCON->MAINCLKSEL                = 0x03;                   //set system oscillator to the output of the PLL 
+LPC_SYSCON->MAINCLKUEN                &= ~(1);                 //write a zero to the MAINCLKUEN register,
+                                                                //necessary for MAINCLKSEL to update
+LPC_SYSCON->MAINCLKUEN                |= 1;                     //write a one to the MAINCLKUEN register 
 
 }
