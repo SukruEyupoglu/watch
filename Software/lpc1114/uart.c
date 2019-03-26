1611497b234fe87514e6f7ace8ef1a41a0f7c0f1
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-26 09:32:27 +0300

    Update uart.c

--- Software/lpc1114/uart.c
+++ Software/lpc1114/uart.c
@@ -4,30 +4,30 @@ void uart_init(void)
 	//	ENABLE IOCON CLK
 	LPC_SYSCON->SYSAHBCLKCTRL		        |=	(1 << 16);
 	//	IOCONFIG UART SETTINGS
-	LPC_IOCON->PIO1_6 					        =	0x1;				//RX
-	LPC_IOCON->PIO1_7 					        =	0x1; 				//TX
+	LPC_IOCON->PIO1_6 			        =	0x1;				//RX
+	LPC_IOCON->PIO1_7 				=	0x1; 				//TX
 	//	ENABLE UART CLK
-	LPC_SYSCON->SYSAHBCLKCTRL 			    |=	(1 << 12);
+	LPC_SYSCON->SYSAHBCLKCTRL 			|=	(1 << 12);
 	//	UART CLK DIVIDER AND UART_CLK_ON
 	LPC_SYSCON->UARTCLKDIV			        =	0x4;
 	//	8 BIT AND OTHER NECESSERY SETTINGS
-	LPC_UART->LCR					              =	3	|	128;
-	LPC_UART->DLM					              =	0x0;
-	LPC_UART->DLL					              =	0x4;
-	LPC_UART->LCR					              =	0x3;
+	LPC_UART->LCR					=	3	|	128;
+	LPC_UART->DLM					=	0x0;
+	LPC_UART->DLL					=	0x4;
+	LPC_UART->LCR					=	0x3;
 	//	MULVAL DIADDVAL
-	LPC_UART->FDR					              =	(8 << 4)	|	5;
+	LPC_UART->FDR					=	(8 << 4)	|	5;
 	//	UART TX-RX FIFO ENABLE
-	LPC_UART->FCR					              =	0x1;
+	LPC_UART->FCR					=	0x1;
 	// UART MODEM CONTROL DISABLE
-	LPC_UART->MCR					              =	0x0;
+	LPC_UART->MCR					=	0x0;
 	//	UART TRANSMIT ENABLE
-	LPC_UART->TER					              =	(1 << 7);
+	LPC_UART->TER					=	(1 << 7);
 	//	UART AUTOBAUD ENABLE
-	//	LPC_UART->ACR				            |=	0x1;
+	//	LPC_UART->ACR				|=	0x1;
 	//	UART IRQ SETTINS
 	//  NVIC_EnableIRQ(UART_IRQn);
-	//  LPC_UART->IER				            =	0x1;
+	//  LPC_UART->IER				=	0x1;
 }
 void uart_send(unsigned char x)
 {
