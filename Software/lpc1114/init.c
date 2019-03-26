17f4a8047a2e8d142d852417a81375e39119bb61
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-26 09:10:04 +0300

    Update init.c

--- Software/lpc1114/init.c
+++ Software/lpc1114/init.c
@@ -14,7 +14,9 @@ LPC_SYSCON->SYSPLLCLKUEN              &= ~(1);                  //write a zero t
 LPC_SYSCON->SYSPLLCLKUEN              |= 1;                     //write a one to SYSPLLUEN register,
                                                                 //necessary for SYSPLLCLKSEL to update
 LPC_SYSCON->PDRUNCFG                  |= (1<<7);                //power down the PLL before changing divider values
-LPC_SYSCON->SYSPLLCTRL                = 0x23;                   //set MSEL = 0x00011 and PSEL = 0x01
+LPC_SYSCON->SYSPLLCTRL                = 0x23;                   //set MSEL = 0x00011 and PSEL = 0x01 --> 48MHZ --> 0x23
+                                                                //set MSEL = 0x00010 and PSEL = 0x10 --> 36MHZ --> 0x42
+                                                                //set MSEL = 0x00001 and PSEL = 0x10 --> 24MHZ --> 0x41
 LPC_SYSCON->PDRUNCFG                  &= ~(1<<7);               //power up PLL after divider values changed 
 while((LPC_SYSCON->SYSPLLSTAT & 1) == 0);                       //wait for PLL to lock
 LPC_SYSCON->MAINCLKSEL                = 0x03;                   //set system oscillator to the output of the PLL 
