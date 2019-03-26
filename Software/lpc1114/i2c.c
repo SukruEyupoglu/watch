8b480a54fa7910dd47d7c81aef37019f43ab6315
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-26 09:40:00 +0300

    Update i2c.c

--- Software/lpc1114/i2c.c
+++ Software/lpc1114/i2c.c
@@ -5,16 +5,17 @@ void i2c_init(void)
 	LPC_SYSCON->SYSAHBCLKCTRL		|=	(1 << 16);
 
 	//	IOCONFIG I2C SETTINGS
-	LPC_IOCON->PIO0_4 				=	0x1;
-	LPC_IOCON->PIO0_5 				=	0x1;
+	LPC_IOCON->PIO0_4 			=	0x1;
+	LPC_IOCON->PIO0_5 			=	0x1;
 	//	I2C CLK
 	LPC_SYSCON->SYSAHBCLKCTRL		|=	(1 << 5);
 	LPC_SYSCON->PRESETCTRL			|=	0x2;
 	LPC_I2C->CONSET					|=	(1 << 6);
 	//	400 khz setting = 3C---100 khz setting = F0
-	LPC_I2C->SCLH					=	0x3C;
-	LPC_I2C->SCLL					=	0x3C;
+	LPC_I2C->SCLH				=	0x3C;
+	LPC_I2C->SCLL				=	0x3C;
 }
+/* this must be re written
 void i2c_write(unsigned char reg,unsigned char data)
 {
 	LPC_I2C->CONCLR				=	(1 << 5) | (1 << 3) | (1 << 2);
@@ -45,11 +46,11 @@ unsigned char i2c_read(unsigned char reg)
 	LPC_I2C->DAT				=	reg;
 	LPC_I2C->CONCLR				=	(1 << 3);
 	while(!((LPC_I2C->STAT == 0x30) | (LPC_I2C->STAT == 0x28)));
-	LPC_I2C->CONSET					|=	(1 << 4);
+	LPC_I2C->CONSET				|=	(1 << 4);
 	delay(500);
-	LPC_I2C->CONCLR					|=	(1 << 6);
+	LPC_I2C->CONCLR				|=	(1 << 6);
 	delay(500);
-	LPC_I2C->CONSET					|=	(1 << 6) |	(1 << 5);
+	LPC_I2C->CONSET				|=	(1 << 6) |	(1 << 5);
 	while(LPC_I2C->STAT != 0x08);
 	LPC_I2C->DAT				=	0x43;
 	LPC_I2C->CONCLR				=	(1 << 5) | (1 << 3);
@@ -63,3 +64,4 @@ unsigned char i2c_read(unsigned char reg)
 	delay(500);
 	return data;
 }
+*/
