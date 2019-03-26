f2c95fd3a7f2a5a378b20e758cf273a7efcc8211
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-26 09:21:45 +0300

    Update spi.c

--- Software/lpc1114/spi.c
+++ Software/lpc1114/spi.c
@@ -1,14 +1,14 @@
 void spi_init(void)
 {
 	//	ENABLE IOCON CLK
-	LPC_SYSCON->SYSAHBCLKCTRL		          |=	(1 << 16);
+	LPC_SYSCON->SYSAHBCLKCTRL		        |=	(1 << 16);
 	//	ENABLE GPIO CLK
-	LPC_SYSCON->SYSAHBCLKCTRL			        |=	(1 << 6);
-  //	IOCONFIG SPI SETTINGS
-	LPC_IOCON->PIO0_8					            =	0x1;                //MISO
-	LPC_IOCON->PIO0_9					            =	0x1;                //MOSI
-	LPC_IOCON->SWCLK_PIO0_10		        	=	0x2;                //SCK
-	LPC_IOCON->PIO2_11				          	=	0x0;                //CSN
+	LPC_SYSCON->SYSAHBCLKCTRL			|=	(1 << 6);
+	//	IOCONFIG SPI SETTINGS
+	LPC_IOCON->PIO0_8		            	=	0x1;                //MISO
+	LPC_IOCON->PIO0_9		            	=	0x1;                //MOSI
+	LPC_IOCON->SWCLK_PIO0_10	        	=	0x2;                //SCK
+	LPC_IOCON->PIO2_11		          	=	0x0;                //CSN
 	//	ENABLE SPI CLK
 	LPC_SYSCON->SYSAHBCLKCTRL 		      	|=	(1 << 11);
 	//	SPI CLK DIVIDER SYSTEMCORECLK / SSP0CLKDIV = SPI CLK
@@ -17,7 +17,14 @@ void spi_init(void)
 	//	CLEAR SPI0 RESET
 	LPC_SYSCON->PRESETCTRL		          	|=	0x01;
 	//	SET 8 BIT TRANSFER
-	LPC_SSP0->CR0					                |=	0x07;
+	LPC_SSP0->CR0			                |=	0x07;
 	//	ENABLE SPI
-	LPC_SSP0->CR1					                |=	0x02;
+	LPC_SSP0->CR1			                |=	0x02;
+}
+unsigned char spi(unsigned char TX_Data)
+{
+	while ((LPC_SSP0->SR & (SSPSR_TNF | SSPSR_BSY)) != SSPSR_TNF);
+	LPC_SSP0->DR = TX_Data;
+	while ((LPC_SSP0->SR & (SSPSR_BSY | SSPSR_RNE)) != SSPSR_RNE);
+	return LPC_SSP0->DR;
 }
