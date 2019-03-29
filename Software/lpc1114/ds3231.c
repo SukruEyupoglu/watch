37e041ac92cd03701275e0abe6b090a0967eaa68
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-03-29 16:49:01 +0300

    Update and rename DS3231.c to ds3231.c

--- Software/lpc1114/ds3231.c
+++ Software/lpc1114/ds3231.c
@@ -1,9 +1,9 @@
-unsigned char time_to_reg(unsigned char time)
+unsigned char time2reg(unsigned char time)
 {
     return (((time / 10) << 4) + (time % 10));
 }
 
-unsigned char reg_to_time(unsigned char reg)
+unsigned char reg2time(unsigned char reg)
 {
     return (((reg >> 4) * 10) + (reg % 16));
 }
