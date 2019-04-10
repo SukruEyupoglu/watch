540efb1e7890bd2dfe25d69da08551ab0776aaf2
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-04-10 16:26:53 +0300

    Create i2c1.c

---
+++ Software/lpc1114/i2c1.c
@@ -0,0 +1,33 @@
+unsigned char i2c_write_1_char(unsigned char addr,unsigned char reg,unsigned char data)
+{
+  LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
+	while(LPC_I2C->STAT != I2CSTAT_START_0x08);
+	LPC_I2C->DAT				=	addr;
+	LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);
+	while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x20) | (LPC_I2C->STAT == I2CSTAT_ACK_0x18)));
+  if(LPC_I2C->STAT == I2CSTAT_ACK_0x18)
+  {
+	  LPC_I2C->DAT				=	reg;
+	  LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+  }
+  else 
+  {
+	  LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	  LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    return 1;
+  }
+	while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x30) | (LPC_I2C->STAT == I2CSTAT_ACK_0x28)));
+  if(LPC_I2C->STAT == I2CSTAT_ACK_0x28)
+  {
+	  LPC_I2C->DAT				=	data;
+    LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	  LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+  }
+  else 
+  {
+	  LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	  LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    return 1;
+  }
+  return 0;
+}
