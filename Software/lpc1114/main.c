f6bfadf44d7bf4f99470b82a009df6b61f2633d4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-04-12 09:33:59 +0300

    Update main.c

--- Software/lpc1114/main.c
+++ Software/lpc1114/main.c
@@ -8,8 +8,8 @@ i2c_init();
 spi_init();
 while(1){
 i2c_read_multi_char(0xD0,0xD1,0x00,saat[19]);
-watch_reg = saat[2];
-minute_reg = saat[1];
+watch_reg   = reg2time(saat[2]);
+minute_reg  = reg2time(saat[1]);
 led_write(watch_reg,minute_reg,0);
 }
 return 0;
