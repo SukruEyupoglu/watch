4bf271299836d6760688370b60fa4948a635a50b
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-07 10:20:27 +0300

    Update e2prom.c

--- Software/lpc1114/e2prom.c
+++ Software/lpc1114/e2prom.c
@@ -3,7 +3,7 @@
 #define extra_data_start_addr 0x0727
 #define extra_data_last_addr 0x4095
 
-void i2c_100khz_init(void)
+void i2c_100khz_e2prom_init(void)
 {
 	//	ENABLE IOCON CLK
 	LPC_SYSCON->SYSAHBCLKCTRL		|=	(1 << 16);
@@ -20,5 +20,53 @@ void i2c_100khz_init(void)
 	LPC_I2C->SCLL					=	0xF0;
 }
 
-
+unsigned char i2c_write_e2prom_32byte_1page(unsigned char ic_addr,unsigned short write_start_addr,unsigned char data[32])
+{
+unsigned char i;
+LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
+while(LPC_I2C->STAT != I2CSTAT_START_0x08);
+LPC_I2C->DAT					=	ic_addr;
+LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x20) | (LPC_I2C->STAT == I2CSTAT_ACK_0x18)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x18)
+	{
+	LPC_I2C->DAT				=	( write_start_addr >> 8 );
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+	}
+else 
+	{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+	}
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x30) | (LPC_I2C->STAT == I2CSTAT_ACK_0x28)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x28)
+	{
+	LPC_I2C->DAT				=	( write_start_addr & 0xF ); 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+}
+else 
+{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+}
+for(i = 0 ; i < 32 ; i++)
+{	
+	while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x30) | (LPC_I2C->STAT == I2CSTAT_ACK_0x28)));
+	if(LPC_I2C->STAT == I2CSTAT_ACK_0x28)
+		{
+		LPC_I2C->DAT				=	data[i];
+		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+	}
+	else 
+	{
+		LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+		return 1;
+	}
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4;
+}
+return 0;
+}
 
