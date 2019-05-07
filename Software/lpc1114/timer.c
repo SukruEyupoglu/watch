0241270db44d73a0942e38fca9ab20cf72bdd0b6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-07 15:01:58 +0300

    Update timer.c

--- Software/lpc1114/timer.c
+++ Software/lpc1114/timer.c
@@ -3,3 +3,15 @@ void timer_0_16_init(void)
 LPC_SYSCON->SYSAHBCLKCTRL |= (1<<7);
 LPC_TMR16B0->PR  = System_Core_Clock/1000000;
 }
+void systick_second_sleep(unsigned char second)
+{
+  
+}
+
+
+
+
+
+
+
+
