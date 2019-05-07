426a691fae7313ee1295b696499d609a3d70b808
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-07 13:59:50 +0300

    Update main.c

--- Software/lpc1114/main.c
+++ Software/lpc1114/main.c
@@ -1,16 +1,16 @@
 int main(void)
 {
 unsigned char saat[19];
-unsigned char watch_reg;
-unsigned char minute_reg;
+ds_t ds3231;
 main_init();
 i2c_init();
 spi_init();
-while(1){
 i2c_read_multi_char(0xD0,0xD1,0x00,saat[19]);
-watch_reg   = reg2time(saat[2]);
-minute_reg  = reg2time(saat[1]);
+ds3231.minute = reg2time(saat[1]);
+ds3231.hour__am_pm = reg2time(saat[2]);
 led_write(watch_reg,minute_reg,0);
+while(1){
+
 }
 return 0;
 }
