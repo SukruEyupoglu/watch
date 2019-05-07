6e2fb7dc8d40cf1abfea9b5594880d3a0ec3ad21
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-07 11:13:50 +0300

    Update e2prom.c

--- Software/lpc1114/e2prom.c
+++ Software/lpc1114/e2prom.c
@@ -114,4 +114,54 @@ else
 }
 return 0;
 }
-
+unsigned char i2c_read_e2prom_1_char(unsigned char ic_addr,unsigned short read_addr,unsigned char * data)
+{
+LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
+while(LPC_I2C->STAT != I2CSTAT_START_0x08);
+LPC_I2C->DAT					=	ic_addr;
+LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x20) | (LPC_I2C->STAT == I2CSTAT_ACK_0x18)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x18)
+	{
+	LPC_I2C->DAT				=	( read_addr >> 8 );
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+	}
+else 
+	{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+	}
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x30) | (LPC_I2C->STAT == I2CSTAT_ACK_0x28)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x28)
+	{
+	LPC_I2C->DAT				=	( read_addr & 0xF );
+	LPC_I2C->CONSET = I2CONSET_RSTA_BIT5;
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+}
+else 
+{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+}
+while(LPC_I2C->STAT != I2CSTAT_START_0x10);
+LPC_I2C->DAT					=	ic_addr + 1;
+LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);	
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x48) | (LPC_I2C->STAT == I2CSTAT_ACK_0x40)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x40)
+	{
+	// LPC_I2C->CONSET = I2CONSET_AA_BIT2; // IF MULTIBYTE READ USE THIS
+	}
+else 
+	{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+	}
+while(LPC_I2C->STAT != I2CSTAT_ACK_0x58);
+*data = LPC_I2C->DAT
+LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+return 0;	
+}
