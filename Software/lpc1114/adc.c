dfdf4e8d6235cbabb1882d7e977e44259a533f1a
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-23 12:05:52 +0300

    Update adc.c

--- Software/lpc1114/adc.c
+++ Software/lpc1114/adc.c
@@ -15,6 +15,13 @@ void adc_init(void)
 	LPC_IOCON->R_PIO1_1 		            	=	0x2;
     //  PIR ADC SETTING
 	LPC_IOCON->R_PIO1_3 		            	=	0x2;
+	// ADC CONTROL REGISTER
+	// ACTIVATE AD0,AD1,AD2,AD4
+	LPC_ADC->CR					|=	(1 << 0) | (1 << 1) | (1 << 2) | (1 << 4);
+	// SET ADC CLK DIVIDER TO (PCLK / (CLKDIV +1)) 100KHZ --> 12000000/120=100000 --> CLKDIV = (120 - 1) = 0x77
+	LPC_ADC->CR					|=	(0x77 << 8);
+	// START CONVERSION
+	//LPC_ADC->CR					|=	(1 << 24);
 	
 }
 
