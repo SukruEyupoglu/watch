ebec1b47469f5936af580dd56b5110fa4ebf5f65
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-23 11:04:15 +0300

    Update adc.c

--- Software/lpc1114/adc.c
+++ Software/lpc1114/adc.c
@@ -1,18 +1,21 @@
 void adc_init(void)
 {
-  //	ENABLE IOCON CLK
+  	//	ENABLE IOCON CLK
 	LPC_SYSCON->SYSAHBCLKCTRL		        |=	(1 << 16);
 	//	ENABLE ADC CLK
-	LPC_SYSCON->SYSAHBCLKCTRL			      |=	(1 << 13);
+	LPC_SYSCON->SYSAHBCLKCTRL			|=	(1 << 13);
+	//	POWER UP ADC
+	LPC_SYSCON->PDRUNCFG				&=	~(1 << 4);
 	//	IOCONFIG ADC SETTINGS
     //  MIC ADC SETTING
 	LPC_IOCON->R_PIO0_11		            	=	0x2;
     //  TIME UP DOWN ADC SETTING
-	LPC_IOCON->R_PIO1_0		            	  =	0x2;
+	LPC_IOCON->R_PIO1_0		            	=	0x2;
     //  LIGHT UP DOWN ADC SETTING
 	LPC_IOCON->R_PIO1_1 		            	=	0x2;
     //  PIR ADC SETTING
 	LPC_IOCON->R_PIO1_3 		            	=	0x2;
+	
 }
 
 
