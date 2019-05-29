c3e46908589249226dee9aa2e6ceea53aefc408d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-05-29 10:15:28 +0300

    Update main.c

--- Software/lpc1114/main.c
+++ Software/lpc1114/main.c
@@ -1,3 +1,5 @@
+#include "LPC11xx.h"
+
 #define setting_alarm_button_pressed (LPC_GPIO1->DATA & (1 << 5))
 #define setting_clk_button_pressed (LPC_GPIO1->DATA & (1 << 11))
 
@@ -7,21 +9,22 @@ volatile unsigned char tick_second = 10;
 
 int main(void)
 {
-// DS3231 HAS 19 REGISTER ADDRESS
-unsigned char saat[19];
-
-ds_t ds3231;
-
-main_init();
-i2c_init();
-spi_init();
-systick_init();
+  // DS3231 HAS 19 REGISTER ADDRESS
+  unsigned char saat[19];
+  // FOR CONVERTING DS REGISTERS TO REASONABLE DATA
+  ds_t ds3231;
+  //  INIT ALL NECESSARY FUNCTIONS
+  main_init();
+  i2c_init();
+  spi_init();
+  systick_init();
   
-//READ ALL REGISTER AND SAVE TO ARRAY  
-i2c_read_multi_char(0xD0,0xD1,0x00,saat[19]);
-
-ds3231.minute = reg2time(saat[1]);
-ds3231.hour__am_pm = reg2time(saat[2]);
+  //READ ALL REGISTER AND SAVE TO RAW ARRAY  
+  i2c_read_multi_char(0xD0,0xD1,0x00,saat[19]);
+  // CONVERT RAW REGISTER DATA TO REASONABLE DATA
+  raw_to_ds_t(ds3231,saat[19]);
+  //  CHECK ALL ALARMS
+  check_alarm(ds3231);  
   
 //WRITE MINUTE AND HOUR
 led_write(ds3231.hour__am_pm,ds3231.minute,0);
