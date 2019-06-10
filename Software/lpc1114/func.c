3e6c32190ad5ac30f5f5f445605fd9ccacb80df5
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-06-10 16:24:27 +0300

    Update func.c

--- Software/lpc1114/func.c
+++ Software/lpc1114/func.c
@@ -16,18 +16,44 @@ void alert(unsigned int freq)
 }
 void setting_clk_on(void)
 {
-	unsigned char s;
-	tick_second = 50;
+	unsigned char s,t = 0;
+	tick_second = 100;
+	delay(blink_time);
 	while(1)
 	{
+	if(t == 0)
+	{
+	led_write(ds3231.hour__am_pm,ds3231.minute,0);
+	delay(blink_time);
+	led_write(ds3231.hour__am_pm,0,0);
+	delay(blink_time);
+	}
+	else
+	{
+	led_write(ds3231.hour__am_pm,ds3231.minute,0);
+	delay(blink_time);
+	led_write(100,ds3231.minute,0);
+	delay(blink_time);		
+	}
+	if(setting_clk_button_pressed == 0)
+	{
+		if(t == 0)
+		{
+			t = 1;
+		}
+		else
+		{
+			t = 0;
+		}
+	}
 	s = adc_1_time_up_down_read();
 	if(s == 5)
   	{
-		increase_hour();
+		increase_minute();
   	}
 	if(s == 20)
 	{
-		reduce_hour();
+		reduce_minute();
 	}
 	}
 
@@ -35,11 +61,12 @@ void setting_clk_on(void)
 }
 void setting_alarm_on(void)
 {
-	tick_second = 50;
+	tick_second = 100;
 	
 }
 void stop_alarm(void)
 {
+	LPC_GPIO2->DATA &= ~(1 << 3);
 	alarm_status = 0;	
 }
 void sleep(void)
