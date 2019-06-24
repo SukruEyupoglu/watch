79834669e03ab9f014c2b9b3acf174d7f38c3370
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-06-24 13:31:58 +0300

    Update led.c

--- Software/lpc1114/led.c
+++ Software/lpc1114/led.c
@@ -1,4 +1,31 @@
-void led_hex_write(unsigned int low_32_bit,unsigned int high_28_bit,unsigned int digit)
+void led_hex_write(unsigned long long int circle,unsigned int digit)
 {
-
+  unsigned char led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+  unsigned char f,x;
+  for(f = 0 ; f < 8 ; f++)
+  {
+    circle >>= (f * 8);
+    for(x = 0 ; x < 8 ; x++)
+    {
+      if((circle & (1 << x)) != 0)
+      {
+        led[f] |= 1;
+      }
+    }    
+  }
+  for(f = 0 ; f < 4 ; f++)
+  {
+    digit >>= (f * 8);
+    for(x = 0 ; x < 8 ; x++)
+    {
+      if((digit & (1 << x)) != 0)
+      {
+        led[f + 8] |= 1;
+      }
+    }    
+  }  
+  for(f = 0 ; f < 12 ; f++)
+  {
+    spi( led[f] );
+  }
 }
