1025b618c9349866fb455b81972cd721f39a23fe
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-06-27 09:17:34 +0300

    Update external_flash.c

--- Software/lpc1114/external_flash.c
+++ Software/lpc1114/external_flash.c
@@ -4,6 +4,10 @@
 // spi_init();
 void erase_all_external_spi_flash(void)
 {
+  CS_LOW;
   spi(WRITE_ENABLE_REGISTER);  // for write anythink set this register
-  spi(CHIP_ERASE_COMMAND);  // for erase all chip 
+  CS_HIGH;
+  CS_LOW;
+  spi(CHIP_ERASE_COMMAND);  // for erase all chip
+  CS_HIGH;
 }
