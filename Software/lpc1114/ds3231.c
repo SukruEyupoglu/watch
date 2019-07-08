4e317a35784669e3ac696dd00ba867911a96dafc
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-08 15:01:22 +0300

    Update ds3231.c

--- Software/lpc1114/ds3231.c
+++ Software/lpc1114/ds3231.c
@@ -7,68 +7,67 @@ unsigned char reg2time(unsigned char reg)
 {
     return (((reg >> 4) * 10) + (reg % 16));
 }
-/*
-void led_write(unsigned char watch,unsigned char minute,unsigned char ref)
+unsigned char i2c(unsigned char addr,unsigned char * data,unsigned int size)
 {
-    unsigned char i,j,l,f;
-    unsigned char x,v;
-    unsigned char y = (minute / 8);
-    unsigned char z = (minute % 8);
-    unsigned char p = (watch / 10);
-    unsigned char q = (watch % 10);
-    unsigned char m = (ref    / 8);
-    unsigned char n = (ref    % 8);
-    unsigned char n7[20] = {0xFF,0xF,0x3C,0x0,0xCF,0x33,0xFF,0x30,0x3C,0x3C,0xF3,0x3C,0xF3,0x3F,0x3F,0x0,0xFF,0x3F,0xFF,0x3C};
-    led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
-    if ((minute < 60) & (ref == 0))
+    if((addr % 10) == 0)
     {
-        for(i=0;i<y;i++)
-        {
-            led[i] = 0xFF;
-        }
-        for(j=0;j<z;j++)
-        {
-            x |= (1 << j);
-        }
-        led[i] |= x;
+        
     }
-    if ((minute < 60) & (ref > 0) & (ref < 60) & (ref < minute))
+    else
     {
-        for(i=0;i<y;i++)
-        {
-            if(i > m)
-            {
-            led[i] = 0xFF;
-            }
-        }
-        for(l=0;l<n;l++)
-        {
-            v |= (1 << l);
-        }        
-        led[(m*8)] &= ~v;
-        for(j=0;j<z;j++)
-        {
-            x |= (1 << j);
-        }
-        led[i] |= x;
+        
     }
-if(watch < 100)
+unsigned char i,
+LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
+while(LPC_I2C->STAT != I2CSTAT_START_0x08);
+LPC_I2C->DAT					=	w_addr;
+LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x20) | (LPC_I2C->STAT == I2CSTAT_ACK_0x18)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x18)
+	{
+	LPC_I2C->DAT				=	start_reg;
+    	LPC_I2C->CONSET = I2CONSET_RSTA_BIT5; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+	}
+else 
+	{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+	}
+while(LPC_I2C->STAT != I2CSTAT_START_0x10);
+LPC_I2C->DAT					=	r_addr;
+LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);	
+while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x48) | (LPC_I2C->STAT == I2CSTAT_ACK_0x40)));
+if(LPC_I2C->STAT == I2CSTAT_ACK_0x40)
+	{
+	LPC_I2C->CONSET = I2CONSET_MULTIBYTE_AA_BIT2;
+	}
+else 
+	{
+	LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    	return 1;
+	}
+for(i=0;i<18;i++)
 {
-    led[8] = n7[(p*2)];
-    led[9] = n7[(p*2)+1];
-    led[10] = n7[(q*2)];
-    led[11] = n7[(q*2)+1];
+	while(LPC_I2C->STAT != I2CSTAT_ACK_0x50);
+	if(LPC_I2C->STAT == I2CSTAT_ACK_0x50)
+		{
+		data[i] = LPC_I2C->DAT;
+		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
+		}
+	else 
+		{
+		LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+    		return 1;
+		}
 }
-else
-{
-    led[8] = 0;
-    led[9] = 0;
-    led[10] = 0;
-    led[11] = 0;    
+LPC_I2C->CONCLR = I2CONCLR_MULTIBYTE_AAC_BIT2;
+while(LPC_I2C->STAT != I2CSTAT_ACK_0x58);
+data[i] = LPC_I2C->DAT;
+LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+return 0;		
 }
-for(f=0;f<12;f++)
-{
-    spi(led[f]);
-}
-}
-*/
