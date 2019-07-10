036eb86591557ace92bea51a52f2e53f5627b91b
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-10 14:40:42 +0300

    Update ds3231.c

--- Software/lpc1114/ds3231.c
+++ Software/lpc1114/ds3231.c
@@ -7,6 +7,25 @@ unsigned char reg2time(unsigned char reg)
 {
     return (((reg >> 4) * 10) + (reg % 16));
 }
+#define I2CONSET_STA_BIT5 (1 << 5)
+#define I2CONCLR_STAC_BIT5 (1 << 5)
+#define I2CONCLR_SIC_BIT3 (1 << 3)
+#define I2CONSET_STO_BIT4 (1 << 4)
+#define I2CONSET_RSTA_BIT5 (1 << 5)
+#define I2CONSET_AA_BIT2 (1 << 2)
+#define I2CONCLR_MULTIBYTE_AAC_BIT2 (1 << 2)
+
+#define I2CSTAT_START_0x08 0x08
+#define I2CSTAT_NACK_0x20 0x20
+#define I2CSTAT_ACK_0x18 0x18
+#define I2CSTAT_NACK_0x30 0x30
+#define I2CSTAT_ACK_0x28 0x28
+#define I2CSTAT_START_0x10 0x10
+#define I2CSTAT_NACK_0x48 0x48
+#define I2CSTAT_ACK_0x40 0x40
+#define I2CSTAT_ACK_0x50 0x50
+#define I2CSTAT_ACK_0x58 0x58
+
 unsigned char i2c(unsigned char addr,unsigned char reg,unsigned char read_or_write,unsigned char * data,unsigned int size)
 {
 	LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
