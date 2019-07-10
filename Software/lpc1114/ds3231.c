3ea20df835696e12c27bb64ef48e9c1d9b9619d6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-10 10:40:33 +0300

    Update ds3231.c

--- Software/lpc1114/ds3231.c
+++ Software/lpc1114/ds3231.c
@@ -7,7 +7,7 @@ unsigned char reg2time(unsigned char reg)
 {
     return (((reg >> 4) * 10) + (reg % 16));
 }
-unsigned char i2c(unsigned char addr,unsigned char * data,unsigned int size)
+unsigned char i2c(unsigned char addr,unsigned char start_reg,unsigned char * data,unsigned int size)
 {
     if((addr % 10) == 0)
     {
@@ -15,12 +15,9 @@ unsigned char i2c(unsigned char addr,unsigned char * data,unsigned int size)
     }
     else
     {
-        
-    }
-unsigned char i,
 LPC_I2C->CONSET = I2CONSET_STA_BIT5; 
 while(LPC_I2C->STAT != I2CSTAT_START_0x08);
-LPC_I2C->DAT					=	w_addr;
+LPC_I2C->DAT					=	addr; // write = 0
 LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);
 while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x20) | (LPC_I2C->STAT == I2CSTAT_ACK_0x18)));
 if(LPC_I2C->STAT == I2CSTAT_ACK_0x18)
@@ -36,7 +33,7 @@ else
     	return 1;
 	}
 while(LPC_I2C->STAT != I2CSTAT_START_0x10);
-LPC_I2C->DAT					=	r_addr;
+LPC_I2C->DAT					=	addr + 1; // read = 1
 LPC_I2C->CONCLR = (I2CONCLR_SIC_BIT3 | I2CONCLR_STAC_BIT5);	
 while(!((LPC_I2C->STAT == I2CSTAT_NACK_0x48) | (LPC_I2C->STAT == I2CSTAT_ACK_0x40)));
 if(LPC_I2C->STAT == I2CSTAT_ACK_0x40)
@@ -49,25 +46,31 @@ else
 	LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
     	return 1;
 	}
-for(i=0;i<18;i++)
+while(size)
 {
-	while(LPC_I2C->STAT != I2CSTAT_ACK_0x50);
+	while((LPC_I2C->STAT != I2CSTAT_ACK_0x50) | (LPC_I2C->STAT != I2CSTAT_ACK_0x58);
 	if(LPC_I2C->STAT == I2CSTAT_ACK_0x50)
 		{
-		data[i] = LPC_I2C->DAT;
+		*data = LPC_I2C->DAT;
 		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;
 		}
-	else 
+	      else if(LPC_I2C->STAT == I2CSTAT_ACK_0x58)
+	 	     {
+		      LPC_I2C->CONCLR = I2CONCLR_MULTIBYTE_AAC_BIT2;
+		      *data = LPC_I2C->DAT;
+			LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
+			LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
+			return 0;        
+		      }
+		else 
 		{
 		LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
 		LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
     		return 1;
 		}
+	data++;
+	size--;
+} 
+return 0;        
 }
-LPC_I2C->CONCLR = I2CONCLR_MULTIBYTE_AAC_BIT2;
-while(LPC_I2C->STAT != I2CSTAT_ACK_0x58);
-data[i] = LPC_I2C->DAT;
-LPC_I2C->CONSET = I2CONSET_STO_BIT4; 
-LPC_I2C->CONCLR = I2CONCLR_SIC_BIT3;  
-return 0;		
 }
