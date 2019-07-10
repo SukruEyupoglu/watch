d41610eef0b20b4112565088ff3f741f447ae6f1
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-10 16:33:21 +0300

    Update error.c

--- Software/lpc1114/error.c
+++ Software/lpc1114/error.c
@@ -49,111 +49,105 @@ void write_digit(unsigned char left,unsigned char right)
     case 'G':
     {
       led[8] = 0xD3;
-      led[9] = 0x
+      led[9] = 0x1F;
     }
     break;
     case 'H':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x3C;
+      led[9] = 0x3F;
     }
     break;
     case 'I':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x00;
+      led[9] = 0x0F;
     }
     break;
     case 'J':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xFC;
+      led[9] = 0x00;
     }
     break;
     case 'K':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x24;
+      led[9] = 0x3F;
     }
     break;
     case 'L':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xC0;
+      led[9] = 0x0F;
     }
     break;
     case 'M':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x34;
+      led[9] = 0x3B;
     }
     break;
     case 'N':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x3B;
+      led[9] = 0x07;
     }
     break;
     case 'O':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xDB;
+      led[9] = 0x06;
     }
     break;
     case 'P':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x0F;
+      led[9] = 0x3F;
     }
     break;
     case 'R':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x03;
+      led[9] = 0x0F;
     }
     break;
     case 'S':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xD1;
+      led[9] = 0x2C;
     }
     break;
      case 'T':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0x00;
+      led[9] = 0x3F;
     }
     break;
     case 'U':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xFC;
+      led[9] = 0x0F;
     }
     break;
     case 'V':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xD4;
+      led[9] = 0x0A;
     }
     break;
     case 'Y':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xFD;
+      led[9] = 0x3D;
     }
     break;
     case 'Z':
     {
-      led[8] = 
-      led[9] = 
+      led[8] = 0xD8;
+      led[9] = 0x32;
     }
     break;    
-    
-     
-     
-     
-     
-     
   }
   for(f = 0 ; f < 12 ; f++)
   {
