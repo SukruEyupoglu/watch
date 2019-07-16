240c4273fb9488a6b4cec25da97b150480b4e6b7
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-16 11:25:34 +0300

    Update pwm.c

--- Software/lpc1114/pwm.c
+++ Software/lpc1114/pwm.c
@@ -2,8 +2,8 @@ void tmr16b1_pwm_init(void)
 {
   LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16) | (1 << 8);          // ENABLE IOCON CLK AND TMR16B1 CLK
   LPC_IOCON->PIO1_9 = 0x1;                                    // MAT0 FUNCTION SELECT
-  LPC_TMR16B1->MR3 = duty;                                  // FULL DUTY CYCLE
-  LPC_TMR16B1->MR0 = duty / percent;                                  // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
+  LPC_TMR16B1->MR3 = duty;                                    // FULL DUTY CYCLE
+  LPC_TMR16B1->MR0 = (duty / 100) * percent;                  // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
   LPC_TMR16B1->MCR = (1 << 10);                               // RESET WHEN MR3 MATCH
   LPC_TMR16B1->PWMC = (1 << 0);                               // PWM LOW TO HIGH AT MR0 SETTING
   LPC_TMR16B1->TCR = 0x02;                                    // RESET TIMER
@@ -16,7 +16,7 @@ void brightness_up(void)
   {
     percent++;
   }
-  LPC_TMR16B1->MR0 = duty / percent;                          // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
+  LPC_TMR16B1->MR0 = (duty / 100) * percent;                  // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
   LPC_TMR16B1->TCR = 0x02;                                    // RESET TIMER
   LPC_TMR16B1->TCR = 0x01;                                    // ENABLE TIMER  
 }
@@ -27,7 +27,7 @@ void brightness_down(void)
   {
     percent--;
   }
-  LPC_TMR16B1->MR0 = duty / percent;                          // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
+  LPC_TMR16B1->MR0 = (duty / 100) * percent;                  // DUTY CYCLE START AT LOW AND WHEN MATCH OCCUR SET HIGH
   LPC_TMR16B1->TCR = 0x02;                                    // RESET TIMER
   LPC_TMR16B1->TCR = 0x01;                                    // ENABLE TIMER  
 }
