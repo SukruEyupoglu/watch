140fe8da739d2de606de56003a7efa3b262ccae7
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-21 18:09:28 +0300

    Update setting.c

--- Software/lpc1114/setting.c
+++ Software/lpc1114/setting.c
@@ -1,33 +1,12 @@
 void setting(void)
 {
-  unsigned char d,x,s;
-  tick_second = 255;
-  while(1)
-  {
-    switch( d )
-    {
-      case 0 :
-      {
-        while(1)
-        {
-          s = adc_1_time_up_down_read();
-          x = adc_2_light_up_down_read();
-          if
-          delay(button_delay);
-        }
-      }
-      break;
-      case 1 :
-      {
-        
-      }
-      break;
-      default :
-      {
-        d = 0;
-        return;
-      }
-      break;        
-    }
-  }
+  unsigned char set[60];
+  systick_second_sleep(100);
+  
 }
+
+
+
+
+
+
