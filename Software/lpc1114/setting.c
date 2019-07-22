a7a721e7e9c8384963c6db30fcfc6878976bdb86
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-22 11:34:47 +0300

    Update setting.c

--- Software/lpc1114/setting.c
+++ Software/lpc1114/setting.c
@@ -1,8 +1,20 @@
 void setting(void)
 {
-  unsigned char set[60];
+  unsigned int ee_reg = 1;
+  unsigned char set;
   systick_second_sleep(255);
-  
+  while(1)
+  {
+    if(i2c(eeprom_addr,ee_reg,2,1,&set,1) == ERR)
+    {
+      error();
+    }
+    led_write(set,(60 - ee_reg),((60 - ee_reg) - 1));
+    while(1)
+    {
+      
+    }
+  }
 }
 // void led_write(unsigned char watch,unsigned char minute,unsigned char ref)
 
