bf04aa41daeb568a9827628eb5a6abd62eaf5a8d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-22 13:43:42 +0300

    Update button.c

--- Software/lpc1114/button.c
+++ Software/lpc1114/button.c
@@ -5,6 +5,17 @@
 #define boot_button_pressed (LPC_GPIO0->DATA & (1 << 1))
 #define write_setting_button_pressed (LPC_GPIO3->DATA & (1 << 4))
 
+#define SET_LRM 201
+#define SET_CLK 202
+#define STP_LRM 203
+#define SLP 204
+#define BOO 205
+#define WRT_SET 206
+#define LIG_DWN 207
+#define LIG_UP 208
+#define TM_DWN 209
+#define TM_UP 210
+
 void gpio_init(void)
 {
   	//	ENABLE IOCON CLK
