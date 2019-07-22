d261860bd675f17b5456dc4bb734aded1b4f42bc
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-07-22 11:57:26 +0300

    Update setting.c

--- Software/lpc1114/setting.c
+++ Software/lpc1114/setting.c
@@ -2,6 +2,7 @@ void setting(void)
 {
   unsigned int ee_reg = 1;
   unsigned char set;
+  unsigned char btn;
   systick_second_sleep(255);
   while(1)
   {
@@ -12,11 +13,73 @@ void setting(void)
     led_write(set,(60 - ee_reg),((60 - ee_reg) - 1));
     while(1)
     {
-      
+      btn = check_button();
+      switch(btn)
+      {
+        case 201:
+          {
+            
+          }
+          break;
+        case 202:
+          {
+            
+          }
+          break;
+        case 203:
+          {
+            
+          }
+          break;
+        case 204:
+          {
+            
+          }
+          break;
+        case 205:
+          {
+            
+          }
+          break;
+        case 206:
+          {
+            
+          }
+          break;
+        case 207:
+          {
+            
+          }
+          break;
+        case 208:
+          {
+            
+          }
+          break;
+        case 209:
+          {
+            
+          }
+          break;
+        case 210:
+          {
+            
+          }
+          break;
+        default:
+          {
+            set_blink();
+          }
+          break;
+      }
     }
   }
 }
 // void led_write(unsigned char watch,unsigned char minute,unsigned char ref)
+void set_blink(unsigned char typ,unsigned int dim_time,unsigned int shiny_time)
+{
+  unsigned char bl[12];
+}
 
 
 
