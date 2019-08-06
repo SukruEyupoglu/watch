c55acccd4111f6e5e04656fd84aa64c2291db47d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-08-06 11:24:43 +0300

    Update yak.c

--- Software/lpc1114/yak.c
+++ Software/lpc1114/yak.c
@@ -1,11 +1,43 @@
 // saat seçilirse başlangıçtan saat değerine kadar olan ledler yanar
 #define dakika 0
-// desen seçilirse düzenli bir biçimde 
-// x değeri yanacak ledleri 1 - 60
-// y değeri sönecek ledleri 61 - 120
-// z değeri başlangıç noktasını 121 - 180
-// v değeri biyiş noktasını 181 - 240
+// dolgu seçilirse düzenli bir biçimde 32 bit için 0 - 31
+// x değeri yanacak ledleri 0 - 5
+// y değeri sönecek ledleri 6 - 11
+// z değeri başlangıç noktasını 12 - 17
+// v değeri biyiş noktasını 18 - 23
 // ifade eder
-#define desen 1
 #define dolgu 2
-void daire_yaz(unsigned char dakika,unsigned char desen,unsigned char tip)
+// desen seçilirse rastgele iki adet 32 bit register bit olarak şekillendirilebilir.
+#define desen 1
+
+void daire_yaz(unsigned char dakika,unsigned int dolgu,unsigned int desen_0_31,unsigned int desen_32_59,unsigned char tip)
+{
+  switch (tip)
+  {
+    case 0 :
+    {
+      
+    }
+    break;
+    case 1 :
+    {
+      
+    }
+    break;
+    case 2 :
+    {
+      
+    }
+    break;
+    case 3 :
+    {
+      
+    }
+    break;
+    default :
+    {
+      
+    }
+    break;
+  }
+}
