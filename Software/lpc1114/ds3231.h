c74d9a086fdc7e3b30a267b4ee25d60352385102
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-09-04 09:31:20 +0300

    Create ds3231.h

---
+++ Software/lpc1114/ds3231.h
@@ -0,0 +1,33 @@
+/*
+#define I2CONSET_STA_BIT5 (1 << 5)
+#define I2CONCLR_STAC_BIT5 (1 << 5)
+#define I2CONCLR_SIC_BIT3 (1 << 3)
+#define I2CONSET_STO_BIT4 (1 << 4)
+#define I2CONSET_RSTA_BIT5 (1 << 5)
+#define I2CONSET_AA_BIT2 (1 << 2)
+#define I2CONCLR_MULTIBYTE_AAC_BIT2 (1 << 2)
+
+#define I2CSTAT_START_0x08 0x08
+#define I2CSTAT_NACK_0x20 0x20
+#define I2CSTAT_ACK_0x18 0x18
+#define I2CSTAT_NACK_0x30 0x30
+#define I2CSTAT_ACK_0x28 0x28
+#define I2CSTAT_START_0x10 0x10
+#define I2CSTAT_NACK_0x48 0x48
+#define I2CSTAT_ACK_0x40 0x40
+#define I2CSTAT_ACK_0x50 0x50
+#define I2CSTAT_ACK_0x58 0x58
+// reg addr size is byte number sample eeprom has 16bit addr size
+*/
+
+unsigned char time2reg(unsigned char time);
+unsigned char reg2time(unsigned char reg);
+unsigned char i2c(
+	unsigned char addr,			//device addr char size
+	unsigned int reg,			//if device data storage size maybe 16-24-32 bit
+	unsigned char reg_addr_size,		//if device data storage 16 = 2 , 24 = 3 byte min = 1 per byte
+	unsigned char read_or_write,		//write = 0 , read = 1
+	unsigned char * data,			//address pointer for send or receive
+	unsigned int size);			//size of send or receive data per byte
+
+
