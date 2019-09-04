511e50746130571f90e4bf3b92709f806bfe63f4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-09-04 09:25:18 +0300

    Create button.h

---
+++ Software/lpc1114/button.h
@@ -0,0 +1,24 @@
+/*
+#define setting_alarm_button_pressed (LPC_GPIO1->DATA & (1 << 5))
+#define setting_clk_button_pressed (LPC_GPIO1->DATA & (1 << 11))
+#define stop_alarm_button_pressed (LPC_GPIO2->DATA & (1 << 4))
+#define sleep_button_pressed (LPC_GPIO2->DATA & (1 << 5))
+#define boot_button_pressed (LPC_GPIO0->DATA & (1 << 1))
+#define write_setting_button_pressed (LPC_GPIO3->DATA & (1 << 4))
+
+#define SET_LRM 201
+#define SET_CLK 202
+#define STP_LRM 203
+#define SLP 204
+#define BOO 205
+#define WRT_SET 206
+#define LIG_DWN 207
+#define LIG_UP 208
+#define TM_DWN 209
+#define TM_UP 210
+*/
+
+void gpio_init(void);
+unsigned char check_button(void);
+
+
