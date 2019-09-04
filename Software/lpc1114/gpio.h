ca18bb8b948035ba5fcfed91f08b00f9bbf64eb7
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-09-04 09:46:34 +0300

    Create gpio.h

---
+++ Software/lpc1114/gpio.h
@@ -0,0 +1,6 @@
+//to main file added
+//#define setting_alarm_button_pressed (LPC_GPIO1->DATA & (1 << 5))
+//#define setting_clk_button_pressed (LPC_GPIO1->DATA & (1 << 11))
+
+void gpio_output_init(void);
+
