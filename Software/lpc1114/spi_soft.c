a3941671136a27ce126c2dcc92098d989ce9a556
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-10-08 12:02:45 +0300

    Update spi_soft.c

--- Software/lpc1114/spi_soft.c
+++ Software/lpc1114/spi_soft.c
@@ -1,10 +1,30 @@
 void spi_soft_send(unsigned char x);
+void spi_soft_init(void);
+unsigned char spi_soft_get(void);
+
 #define CLK_HIGH LPC_GPIO1->DATA |= (1 << 2)
 #define CLK_LOW LPC_GPIO1->DATA &= ~(1 << 2)
-#define MOSI_HIGH LPC_GPIO1->DATA |= (1 << 2)
-#define MOSI_LOW LPC_GPIO1->DATA &= ~(1 << 2)
+#define MOSI_HIGH LPC_GPIO1->DATA |= (1 << 0)
+#define MOSI_LOW LPC_GPIO1->DATA &= ~(1 << 0)
 #define MISO LPC_GPIO1->DATA & (1 << 1)
 
+void spi_soft_init(void)
+{
+  //	ENABLE IOCON CLK
+  LPC_SYSCON->SYSAHBCLKCTRL |=	(1 << 16);
+  //	ENABLE GPIO CLK
+  LPC_SYSCON->SYSAHBCLKCTRL |=	(1 << 6);
+  
+  // LPC_IOCON->SWDIO_PIO1_3 = 1; // cs or ss if u need
+  LPC_IOCON->R_PIO1_2 = 1;
+  LPC_IOCON->R_PIO1_1 = 1;
+  LPC_IOCON->R_PIO1_0 = 1;
+  
+  LPC_GPIO1->DIR |= (1 << 0) | (1 << 2);
+  LPC_GPIO1->DIR &= ~(1 << 1);
+  
+  LPC_GPIO1_DATA &= ~((1 << 0) | (1 << 2));
+}
 
 
 
@@ -12,6 +32,10 @@ void spi_soft_send(unsigned char x);
 
 
 void spi_soft_send(unsigned char x)
+{
+  
+}
+unsigned char spi_soft_get(void)
 {
   
 }
