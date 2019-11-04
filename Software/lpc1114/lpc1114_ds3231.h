a8dcbe2c737d498ba01fb659019921570fee7bff
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-04 15:17:17 +0300

    Update lpc1114_ds3231.h

--- Software/lpc1114/lpc1114_ds3231.h
+++ Software/lpc1114/lpc1114_ds3231.h
@@ -1,4 +1,12 @@
-void get_ds3231_data(void);
+
+
+
+
+void read_ds3231_data(void);
 unsigned char time2reg(unsigned char time);
 unsigned char reg2time(unsigned char reg);
 void raw_to_ds_t(ds_t * ds,unsigned char array[19]);
+
+
+
+
