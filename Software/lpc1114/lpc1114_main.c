b1465ade2764be5704ebecc71da5483b9e656eda
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-06 14:42:04 +0300

    Update lpc1114_main.c

--- Software/lpc1114/lpc1114_main.c
+++ Software/lpc1114/lpc1114_main.c
@@ -2,6 +2,29 @@
 
 #include "LPC11xx.h"
 #include "lpc1114_init.h"
+#include "lpc1114_button.h"
+#include "lpc1114_i2c.h"
+#include "lpc1114_spi.h"
+
+// SAAT DATA FROM DS3231 EVERY FUNCTION REACABLE OPTION
+volatile ds_t ds3231;
+
+// FOR LED DATA HOLDER ARRAY EVERY FUNCTION REACABLE OPTION
+volatile unsigned char led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+
+int main(void)
+{
+  init();               // lpc1114_init.h
+  gpio_init();          // lpc1114_button.h
+  i2c_init();           // lpc1114_i2c.h
+  spi_init();           // lpc1114_spi.h
+  
+  
+  
+  while(1);
+  return 0;
+}
+
 
 
 
