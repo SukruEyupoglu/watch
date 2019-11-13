a26a940e5f382417422a2b55426ce32ed0d8e6d8
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-13 11:17:00 +0300

    Create lpc1114_led.h

---
+++ Software/lpc1114/lpc1114_led.h
@@ -0,0 +1,25 @@
+
+
+
+void set_led_write_reg(unsigned char minute,unsigned char hour);
+
+void led_write(void);
+
+void limit_yaz(unsigned char x,unsigned char y);
+void limit_sil(unsigned char x,unsigned char y);
+
+void led_yan(unsigned char xx);
+void led_son(unsigned char xx);
+
+void led_zero(void);
+void led_one(void);
+
+void led_write_digit(unsigned char left,unsigned char right);
+
+
+
+#define ZERO 0
+
+
+
+
