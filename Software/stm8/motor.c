0238228bc9b2263cbdf6a3884ce5c8e0457db8ac
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-14 15:08:11 +0300

    Update motor.c

--- Software/stm8/motor.c
+++ Software/stm8/motor.c
@@ -15,6 +15,44 @@ void find_motor_coil_point(void)
   get_status_of_motor_register_and_reverse_is_first_step of motor();
   
 }
+unsigned char move_to_find_limit_switch(unsigned char next_or_back)
+{
+  unsigned char f;
+  if(next_or_back == NEXT)
+  {
+    while(1)
+    {
+      for(f = 0 ; f < 8 ; f++)
+      {
+        spi(step_type[f]);
+        if(look_at_limit_switch_errors())
+        {
+          return f;
+        }
+      }
+    }
+  }
+  if(next_or_back == BACK)
+  {
+    while(1)
+    {
+      for(f = 8 ; f > 0 ; f--)
+      {
+        spi(step_type[f - 1]);
+        if(look_at_limit_switch_errors())
+        {
+          return f;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+
+
+
+
 
 
 
