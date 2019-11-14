54383add5e72e47687b6d7cdf776dfd2f89b93a4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-14 15:45:58 +0300

    Update spi.h

--- Software/stm8/spi.h
+++ Software/stm8/spi.h
@@ -16,14 +16,16 @@
  */
  
 // spi clock channel select
-#define CLK_CH_M0
-#define CLK_CH_M1
-#define CLK_CH_M2
-#define CLK_CH_M3
-#define CLK_CH_GPIO_OUT
-#define CLK_CH_GPIO_IN
-#define CLK_CH_FLASH
-#define CLK_CH_SRAM
+// stm8s has A1,A2,A3 pins not A0 pin
+// directly write to PA DATA register this calculated values
+#define CLK_CH_M0        0x0
+#define CLK_CH_M1        0x2
+#define CLK_CH_M2        0x4
+#define CLK_CH_M3        0x6
+#define CLK_CH_GPIO_OUT  0x8
+#define CLK_CH_GPIO_IN   0xC
+#define CLK_CH_FLASH     0xA
+#define CLK_CH_SRAM      0xE
 
 void spi_init(void);
 unsigned char spi(unsigned char data);
