f625d3d42379a64b656adeb2281543a452aa7ab6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-14 14:08:02 +0300

    Create stm8.c

---
+++ Software/stm8/stm8.c
@@ -0,0 +1,22 @@
+#include "stm8s.h"
+
+volatile unsigned char motor_1;
+volatile unsigned char motor_2;
+volatile unsigned char motor_3;
+volatile unsigned char motor_4;
+volatile unsigned char gpio_0_to_7;
+
+int main(void)
+{
+
+
+
+  while(1)
+  {
+    
+  }
+
+  return 0;
+}
+
+
