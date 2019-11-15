d2fd58288c1a59dee638ffd5d40a2882f1a43064
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-15 10:32:08 +0300

    Update stm8.c

--- Software/stm8/stm8.c
+++ Software/stm8/stm8.c
@@ -1,5 +1,5 @@
 #include "stm8s.h"
-
+#include "motor.h"
 volatile unsigned char motor_1;
 volatile unsigned char motor_2;
 volatile unsigned char motor_3;
@@ -11,6 +11,8 @@ volatile unsigned char gpio_16_to_23;
 // limit switch errors must be checked before every step
 // only interrupt can make "1" this register
 volatile unsigned char limit_interrupt;
+
+volatile const unsigned char step_queue[8] = {FCF,TCF,SCF,SCFFCB,FCB,TCB,SCB,FCFSCB};
 
 int main(void)
 {
