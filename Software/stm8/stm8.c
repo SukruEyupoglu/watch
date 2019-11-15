0088f2cd37aedf097d87ae79c70b8e73703acb1a
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-15 15:19:55 +0300

    Update stm8.c

--- Software/stm8/stm8.c
+++ Software/stm8/stm8.c
@@ -3,8 +3,12 @@
 
 // motor_status looking for active_motor
 volatile unsigned char motor_status[4] = {SB,SB,SB,SB};
-volatile unsigned char active_motor;
+volatile unsigned char active_motor; // selected active motor
+// motor shift register related data
 volatile const unsigned char step_queue[8] = {FCF,TCF,SCF,SCFFCB,FCB,TCB,SCB,FCFSCB};
+// if there is a limit error look at direction for rescue motor from error
+// immadiately motor must step far away from error
+volatile unsigned char active_motor_direction; // direction 0 is back 1 is next
 
 // gpio output status 
 volatile unsigned char gpio_0_to_7;
