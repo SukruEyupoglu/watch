c31c425bcddbacf5ff1c40cd87084a8f37031f5a
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-15 10:26:02 +0300

    Update motor.c

--- Software/stm8/motor.c
+++ Software/stm8/motor.c
@@ -8,12 +8,158 @@ void release_motor(void)
   latch();
 }
 
-unsigned char motor_next(unsigned char stat,unsigned char double_or_single)
+unsigned char motor_next(unsigned char stat,unsigned char type)
 {
-  
+  switch(type)
+  {
+    case FULL_STEP:
+      {
+        // one by one // "7" is last step total 8 step (0-7)
+        if(stat >= 7)
+        {
+          return 0;
+        }
+        else
+        {
+          return (stat + 1);
+        }
+      }
+      break;
+    case HALF_STEP:
+      {
+        if(stat % 2)
+        {
+          // move 2 by 2 with single numbers (1,3,5,7) 4 step total
+          if(stat >= 7)
+          {
+            return 1;
+          }
+          else
+          {
+            return (stat + 2);
+          }
+        }
+        else
+        {
+          return (stat + 1);
+        }
+      }
+      break;
+    case QUARTER_STEP:
+      {
+        if(stat % 2)
+        {
+          // if sigle number and last number move to start and dual numbers
+          if(stat >= 7)
+          {
+            return 0;
+          }
+          else
+          {
+            return (stat + 1);
+          }
+        }
+        else
+        {
+          // two by two // "6" is last step total 4 step (0,2,4,6)
+          if(stat >= 6)
+          {
+            return 0;
+          }
+          else
+          {
+            return (stat + 2);
+          }
+        }        
+      }
+      break;
+    default:
+      {
+        
+      }
+      break;     
+  }
 }
 
-unsigned char motor_back(unsigned char stat,unsigned char double_or_single)
+unsigned char motor_back(unsigned char stat,unsigned char type)
+{
+  switch(type)
+  {
+    case FULL_STEP:
+      {
+        // one by one // "7" is last step total 8 step (0-7)
+        if(stat == 0)
+        {
+          return 7;
+        }
+        else
+        {
+          return (stat - 1);
+        }
+      }
+      break;
+    case HALF_STEP:
+      {
+        if(stat == 0)
+        {
+          return 7;
+        }
+        if(stat % 2)
+        {
+          // move 2 by 2 with single numbers (1,3,5,7) 4 step total
+          if(stat == 1)
+          {
+            return 7;
+          }
+          else
+          {
+            return (stat - 2);
+          }
+        }
+        else
+        {
+          return (stat - 1);
+        }
+      }
+      break;
+    case QUARTER_STEP:
+      {
+        if(stat % 2)
+        {
+          // if sigle number and last number move to start and dual numbers
+          if(stat == 1)
+          {
+            return 6;
+          }
+          else
+          {
+            return (stat - 1);
+          }
+        }
+        else
+        {
+          // two by two // "6" is last step total 4 step (0,2,4,6)
+          if(stat == 0)
+          {
+            return 6;
+          }
+          else
+          {
+            return (stat - 2);
+          }
+        }        
+      }
+      break;
+    default:
+      {
+        
+      }
+      break;     
+  }
+}
+
+
+unsigned char motor_back(unsigned char stat,unsigned char type)
 {
   
 }
@@ -29,10 +175,11 @@ void hold_motor(unsigned char endurance_amount)
 }
 
 // step_type --> step_queue reduce double point and reduce motor torque
-// 0 or 1 or 2 
-// --> 0 = increase 1 by 1
-// --> 1 = increase 2 by 2 but only one coil
-// --> 2 = increase 2 by 2 but with two coil
+// 0 or 1 or 2 power of steps , not distance (weak steps = long distances)
+// --> 0 = increase 1 by 1 FULL_STEP
+// --> 1 = increase 2 by 2 but only one coil QUARTER_STEP
+// --> 2 = increase 2 by 2 but with two coil HALF_STEP
+// direction 1 = next , 0 = back
 unsigned char motor_move(unsigned char step_count,unsigned char step_type,unsigned char speed,unsigned char direction)
 {
   unsigned char f,mt_sta;
