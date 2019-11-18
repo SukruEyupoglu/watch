d2923898cfda4d604664d115d0bea1cd0c88c325
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-18 17:52:19 +0300

    Update sccb.c

--- Software/lpc1114/sccb.c
+++ Software/lpc1114/sccb.c
@@ -1,3 +1,9 @@
+// this code wrong 
+// because not high low
+// input or low
+// when input,pullup resistor makes high this line
+// when low ,gpio output low used to low
+
 void sccb_init(void)
 {
 SCL_UP;
