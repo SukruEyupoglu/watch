b208006022c32ff2ee45fcd461b925036e141ca6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-21 09:24:10 +0300

    Create uart.c

---
+++ Software/stm8/uart.c
@@ -0,0 +1,18 @@
+#include "stm8s.h"
+#include "uart.h"
+
+void uart_init(void)
+{
+  // ROUND TO NEAREST INTEGER
+  uint16_t div = (F_CPU + BAUDRATE / 2 ) / BAUDRATE;
+  
+  UART1_BRR2 = ( (div >> 8) & 0xF0) + (div & 0x0F);
+  UART1_BRR1 = (div >> 4);
+  
+  // ENABLE TRANSMITTER AND RECEIVER
+  UART1_CR2 = ( (1 << UART1_CR2_TEN) | (1 << UART1_CR2_REN) );
+}
+
+
+
+
