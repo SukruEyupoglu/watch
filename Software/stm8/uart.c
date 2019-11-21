77fca6aaf9b485f68fd8ab8c52563815e3edb188
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-11-21 09:41:08 +0300

    Update uart.c

--- Software/stm8/uart.c
+++ Software/stm8/uart.c
@@ -4,7 +4,7 @@
 void uart_init(void)
 {
   // ROUND TO NEAREST INTEGER
-  uint16_t div = (F_CPU + BAUDRATE / 2 ) / BAUDRATE;
+  uint16_t div = (F_CPU + (BAUDRATE / 2 ) ) / BAUDRATE;
   
   UART1_BRR2 = ( (div >> 8) & 0xF0) + (div & 0x0F);
   UART1_BRR1 = (div >> 4);
