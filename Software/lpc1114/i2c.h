817cb8cdae41ec0849c25331b047c0cd81c957aa
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2019-12-27 11:55:14 +0300

    Update i2c.h

--- Software/lpc1114/i2c.h
+++ Software/lpc1114/i2c.h
@@ -1,20 +1,44 @@
-/*
-//#include "LPC11xx.h"
-// TIMER COUNTER COUNT 1MHZ AT SECOND
-// I2C 400KHZ AT SECOND
-// 1 BYTE = 8 BIT --> WITHOUT START STOP PULSE 400000/8 = 50000 --> 1000000/50000 = 20
-// 1 BYTE TRANFERED AT 20 COUT TO TIMER
-// DOUBLE TIME ENOUGH FOR WAIT 20*2 = 40
-#define I2C_TIMEOUT 40
-unsigned int I2C_ERROR = 0 ;
-*/
+#define CHECK_SI_BIT (!(LPC_I2C->CONSET & (1<<3)))
+#define CLEAR_SI_BIT (LPC_I2C->CONCLR = (1 << 3))
+#define CLEAR_STA_BIT (LPC_I2C->CONCLR = (1 << 5))
+#define SET_STA_BIT (LPC_I2C->CONSET = (1 << 5))
+#define SET_STO_BIT (LPC_I2C->CONSET = (1 << 4))
+#define ERROR 1
+#define OK 0
 
-void i2c_init_timer(void);
-void i2c_write_timer(unsigned char addr,unsigned char reg,unsigned char data);
-unsigned char i2c_read_timer(unsigned char write_addr,unsigned char read_addr,unsigned char reg);
+#define I2CONSET_STA_BIT5 (1 << 5)
+#define I2CONCLR_STAC_BIT5 (1 << 5)
+#define I2CONCLR_SIC_BIT3 (1 << 3)
+#define I2CONSET_STO_BIT4 (1 << 4)
+#define I2CONSET_RSTA_BIT5 (1 << 5)
+#define I2CONSET_AA_BIT2 (1 << 2)
+#define I2CONCLR_MULTIBYTE_AAC_BIT2 (1 << 2)
+
+#define I2CSTAT_START_0x08 0x08
+#define I2CSTAT_NACK_0x20 0x20
+#define I2CSTAT_ACK_0x18 0x18
+#define I2CSTAT_NACK_0x30 0x30
+#define I2CSTAT_ACK_0x28 0x28
+#define I2CSTAT_START_0x10 0x10
+#define I2CSTAT_NACK_0x48 0x48
+#define I2CSTAT_ACK_0x40 0x40
+#define I2CSTAT_ACK_0x50 0x50
+#define I2CSTAT_ACK_0x58 0x58
+
+
 void i2c_init(void);
-void i2c_write(unsigned char addr,unsigned char reg,unsigned char data);
-unsigned char i2c_read(unsigned char write_addr,unsigned char read_addr,unsigned char reg);
+
+unsigned char i2c
+	(
+	unsigned char dev_addr,	//device addr for start comminication , last bit show which read or write. write = 0 read = 1
+	unsigned int reg_addr,	//device reg addr , if device a storage device maybe 16bit or 24bit
+	unsigned char byte_len,	//if device a storage device there is a byte length , how many byte used inside reg value ?
+				                  // 1 byte address size = 1 , 2 byte = 2 , 3 byte = 3 max = 4 byte
+	unsigned char * data,	//address pointer for send or receive
+	unsigned int data_size	//size of send or receive data per byte , how many byte
+	);
+
+
 
 
 
