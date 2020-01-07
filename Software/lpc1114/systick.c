3262328328eeaf4422bb8047839d5e8d932a93c4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-07 12:02:27 +0300

    Update systick.c

--- Software/lpc1114/systick.c
+++ Software/lpc1114/systick.c
@@ -1,9 +1,9 @@
 #include "LPC11xx.h"
-
-#define SYSTEM_CLOCK_FREQUENCY 12 // 12MHZ
+#include "systick.h"
 
 void delay_sys(unsigned int u_sec)
 {
+  // systick max 24 bit === 0xFFFFFF
   if(u_sec < 0xFFFFFF)
   {
   SYSTICK_RVR = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 1; // must be tested "1" maybe too low "5" maybe
@@ -19,6 +19,7 @@ void delay_sys(unsigned int u_sec)
 void SysTick_Handler(void)
 {
   SYSTICK_CSR &= ~( (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT) );
+  // maybe clear interrupt
 }
 
 
