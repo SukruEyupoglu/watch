0336c6b241b1f7111b6b045080c94330b592de87
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-07 10:15:10 +0300

    Create PLATFORMLESS.h

---
+++ Software/lpc1114/PLATFORMLESS.h
@@ -0,0 +1,11 @@
+
+
+
+void delay(volatile unsigned int bekle);
+
+unsigned char check_hex_data(unsigned char data);
+
+unsigned char ascii_to_hex(unsigned char data);
+unsigned char hex_to_ascii(unsigned char data);
+
+
