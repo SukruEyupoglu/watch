a05f4659da43c3bba16f023a3e9c6d25cbe98286
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-07 13:13:48 +0300

    Update systick.c

--- Software/lpc1114/systick.c
+++ Software/lpc1114/systick.c
@@ -6,19 +6,19 @@ void delay_sys(unsigned int u_sec)
   // systick max 24 bit === 0xFFFFFF
   if(u_sec < 0xFFFFFF)
   {
-  SYST_RVR = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 1; // must be tested "1" maybe too low "5" maybe
+    SysTick->LOAD = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 1; // must be tested "1" maybe too low "5" maybe
   }
   else
   {
-    SYST_RVR = 0xFFFFFF;
+    SysTick->LOAD = 0xFFFFFF;
   }
-  SYST_CVR = 0;
-  SYST_CSR |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT);
+  SysTick->VAL = 0;
+  SysTick->CTRL |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT);
   __WFI();  // WAIT FOR INTERRUPT INSTRUCTION (CMSIS)
 }
 void SysTick_Handler(void)
 {
-  SYST_CSR &= ~( (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT) );
+  SysTick->CTRL &= ~( (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT) );
 }
 
 
