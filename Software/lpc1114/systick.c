0d3e97ec25aa03c19c45e16bd939679f1d287d0c
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-07 13:57:25 +0300

    Update systick.c

--- Software/lpc1114/systick.c
+++ Software/lpc1114/systick.c
@@ -6,13 +6,17 @@ void delay_sys(unsigned int u_sec)
   // systick max 24 bit === 0xFFFFFF
   if(u_sec < 0xFFFFFF)
   {
+    // systick start value
     SysTick->LOAD = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 1; // must be tested "1" maybe too low "5" maybe
   }
   else
   {
+    // systick start value
     SysTick->LOAD = 0xFFFFFF;
   }
+  // clear current value
   SysTick->VAL = 0;
+  // enable and set systick timer
   SysTick->CTRL |= (1 << SYSTICK_CLKSOURCE_BIT) | (1 << SYSTICK_TICKINT_BIT) | (1 << SYSTICK_ENABLE_BIT);
   __WFI();  // WAIT FOR INTERRUPT INSTRUCTION (CMSIS)
 }
