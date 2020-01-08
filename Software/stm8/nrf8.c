5effdca50941c56f1d308446f7cef50358bd483d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-08 16:41:50 +0300

    Update nrf8.c

--- Software/stm8/nrf8.c
+++ Software/stm8/nrf8.c
@@ -1,15 +1,27 @@
 void nrf_gpio_init(void)
 {
-  //	NRF IRQ SETTINGS
-	LPC_GPIO2->DIR					&=	~(1 << 2);
-	//	NRF CE SETTINGS
-	LPC_GPIO1->DIR					|=	(1 << 10);
-	LPC_GPIO1->DATA					&=	~(1 << 10);
-	//	NRF CSN SETTINGS
-	LPC_GPIO2->DIR					|=	(1 << 11);
-	LPC_GPIO2->DATA					|=	(1 << 11);
-  
- 	//	100ms delay for nrf24l01 wakeup
+  	//	NRF IRQ SETTINGS
+	//	MAKE DIRECTION INPUT PC4,PC3
+	PC_DDR &= ~( (1 << 3) | (1 << 4) ); // 3 FOR NRF1 --- 4 FOR NRF2
+	//	NRF CE SETTINGS CHIP ENABLE ACTIVES RX - TX MODE --- ACTIVE HIGH 
+	//	RX MODE CE MUST BE HIGH 
+	//	TX MODE CE MUST BE A PULSE MORE THAN 10us -- 4ms DEADLINE FOR CE HIGH TIME
+	//	YOU CAN USE 15us FOR COMMINICATION BEWARE FOR TX MODE DEADLINE
+	PA_DDR |= (1 << 3); // NRF1
+	PD_DDR |= (1 << 3); // NRF2
+	PA_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
+	PD_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
+	PA_ODR &= ~(1 << 3); // OUTPUT LOW
+	PD_ODR &= ~(1 << 3); // OUTPUT LOW
+	//	NRF CSN SETTINGS SPI CHIP SELECT ACTIVE LOW
+	PA_DDR |= (1 << 2); // NRF1
+	PD_DDR |= (1 << 2); // NRF2
+	PA_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
+	PD_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
+	PA_ODR |= (1 << 3); // OUTPUT HIGH
+	PD_ODR |= (1 << 3); // OUTPUT HIGH
+	
+ 	//	100ms delay for nrf24l01 wakeup ---  FROM DATASHEET
 	delay(100000);
 }
 
