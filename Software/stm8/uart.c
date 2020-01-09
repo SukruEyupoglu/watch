acefb8aec5e4f20fa60fe6f5201b6b720d1078a6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-09 14:28:13 +0300

    Update uart.c

--- Software/stm8/uart.c
+++ Software/stm8/uart.c
@@ -9,20 +9,20 @@ void uart_init(void)
   // UART1_BRR1 = (div >> 4);
   
   // no need calculating look at user manual for other bauds
-  // UART_DIV = 0x008B // F_MASTER = 16MHZ
-  UART1_BRR1 = 0x08;
-  UART1_BRR2 = 0x0B;
+  UART_DIV    = 0x008B // F_MASTER = 16MHZ
+  UART1_BRR1  = 0x08;
+  UART1_BRR2  = 0x0B;
   // ENABLE TRANSMITTER AND RECEIVER
   UART1_CR2 = ( (1 << UART1_CR2_TEN) | (1 << UART1_CR2_REN) );
 }
 
-void uart_write(unsigned char data)
+void uart_send(unsigned char data)
 {
   UART1_DR = data;
   while( ! (UART1_SR & (1 << UART1_SR_TC) ) );
 }
 
-unsigned char uart_read(void)
+unsigned char uart_get(void)
 {
   while( ! (UART1_SR & (1 << UART1_SR_RXNE) ) );
   return UART1_DR;
