20ee7b31a34ddab031182b3715e51f4930268e3f
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-09 13:44:28 +0300

    Update nrf8.c

--- Software/stm8/nrf8.c
+++ Software/stm8/nrf8.c
@@ -13,6 +13,15 @@ void delay(volatile unsigned char bekle)
 	{
 		bekle--;
 	}
+	//	delay(0)	1.63us
+	//	delay(1)	2.07us
+	//	delay(2)	2.51us
+	//	delay(3)	2.95us
+	//	delay(4)	3.39us
+	//	delay(5)	3.83us
+	//	delay(6)	4.27us
+	//	delay(20)	10.43us
+	//	delay(255)	113.8us  //MAX
 }
 #define delay_10us delay(20)
 ///////////////////////////////////////////////////////////////////////
