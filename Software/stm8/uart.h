7f633073c2ac21835262773390bcbceaa6f725b3
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-01-10 09:47:31 +0300

    Update uart.h

--- Software/stm8/uart.h
+++ Software/stm8/uart.h
@@ -8,9 +8,17 @@
 
 // 8N1,FLOW CONTROL NONE
 
+#define UART1_CR2_TEN (1 << 3)
+#define UART1_CR2_REN (1 << 2)
+#define UART1_CR2_RIEN (1 << 5)
+#define UART1_SR_TC (1 << 6)
+#define UART1_SR_RXNE (1 << 5)
+
 void uart_init(void);
 void uart_send(unsigned char data);
 unsigned char uart_get(void);
+
+//void uart_isr() __interrupt(UART1_RXC_ISR);
 
 #endif // UART_H
 
