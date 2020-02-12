1b8e1571571a893bc094b86532bfd43d35184919
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-02-12 22:55:37 +0300

    Update led.c

--- Software/lpc1114/led.c
+++ Software/lpc1114/led.c
@@ -1,158 +1,558 @@
-/*
-circle = 60 valid bit + 4 blank bit 
-digit = 14 valid + 2 empty + 14 valid + 2 empty
-unsigned long long int circle , z = 1 , v = 0;
-circle = (z << 0) |     (v << 57) |
-         (z << 1) |     (z << 58) |
-         (v << 2) |     (z << 59);  // 60,61,62,63 is not matter,unvalid.
-vb..
-digit same as circle but 32 bit
-*/
-void latch(void)
+#include "lpc1114_spi.h"
+#include "lpc1114_led.h"
+#include "LPC11xx.h"
+
+// FOR LED DATA HOLDER ARRAY EVERY FUNCTION REACABLE OPTION
+volatile unsigned char led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+
+void set_led_write_reg(unsigned char minute,unsigned char hour)
 {
-         LPC_GPIO2->DATA		|= (1 << 10);
-         LPC_GPIO2->DATA   	&= ~(1 << 10);
-}
-void led_hex_write(unsigned long long int circle,unsigned int digit)
+  // ZERO dan minuteye kadar olan ledleri yakar.
+  limit_yaz(ZERO,minute);
+  // sol digite 10(ON) ve katları, sag digite 10(ON) a kadar olan sayılar
+  led_write_digit((hour / 10),(hour % 10));
+
+void latch(void) // duzeltildi.
 {
-  unsigned char f,x;
-  // led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
-  for(f = 0 ; f < 8 ; f++)
-  {
-    circle >>= (f * 8);
-    for(x = 0 ; x < 8 ; x++)
-    {
-      if((circle & (1 << x)) != 0)
-      {
-        led[f] |= 1;
-      }
-    }    
-  }
-  for(f = 0 ; f < 4 ; f++)
-  {
-    digit >>= (f * 8);
-    for(x = 0 ; x < 8 ; x++)
-    {
-      if((digit & (1 << x)) != 0)
-      {
-        led[f + 8] |= 1;
-      }
-    }    
-  }  
-  for(f = 0 ; f < 12 ; f++)
-  {
-    spi( led[f] );
-  }
-         latch();
+LPC_GPIO2->DATA   	&= ~(1 << 10);
+LPC_GPIO2->DATA		|= (1 << 10);
 }
-void led_write(unsigned char watch,unsigned char minute,unsigned char ref)
-{
-    unsigned char i,j,l,f;
-    unsigned char x,v;
-    unsigned char y = (minute / 8);
-    unsigned char z = (minute % 8);
-    unsigned char p = (watch / 10);
-    unsigned char q = (watch % 10);
-    unsigned char m = (ref    / 8);
-    unsigned char n = (ref    % 8);
-    unsigned char n7[20] = {0xFF,0xF,0x3C,0x0,0xCF,0x33,0xFF,0x30,0x3C,0x3C,0xF3,0x3C,0xF3,0x3F,0x3F,0x0,0xFF,0x3F,0xFF,0x3C};
-    led[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
-    if ((minute < 60) & (ref == 0))
-    {
-        for(i=0;i<y;i++)
-        {
-            led[i] = 0xFF;
-        }
-        for(j=0;j<z;j++)
-        {
-            x |= (1 << j);
-        }
-        led[i] |= x;
-    }
-    if ((minute < 60) & (ref > 0) & (ref < 60) & (ref < minute))
-    {
-        for(i=0;i<y;i++)
-        {
-            if(i > m)
-            {
-            led[i] = 0xFF;
-            }
-        }
-        for(l=0;l<n;l++)
-        {
-            v |= (1 << l);
-        }        
-        led[(m*8)] &= ~v;
-        for(j=0;j<z;j++)
-        {
-            x |= (1 << j);
-        }
-        led[i] |= x;
-    }
-if(watch < 100)
-{
-    led[8] = n7[(p*2)];
-    led[9] = n7[(p*2)+1];
-    led[10] = n7[(q*2)];
-    led[11] = n7[(q*2)+1];
-}
-else
-{
-    led[8] = 0;
-    led[9] = 0;
-    led[10] = 0;
-    led[11] = 0;    
-}
-for(f=0;f<12;f++)
-{
-    spi(led[f]);
-}
-         latch();
-}
-void blink(unsigned char blink_type,unsigned int blink_shiny_time,unsigned int blink_dim_time)
-{
-         unsigned long long int circle = 0;
-         unsigned int digit = 0;
-         unsigned char f,x;
-         for(f = 0 ; f < 8 ; f++)
-         {
-                  for(x = 0 ; x < 8 ; x++)
-                  {
-                           if((led[f] & (1 << x)) != 0)
-                           {
-                                    circle |= (1 << ((f * 8) + x));
-                           }
-                  }
-         }
-         for(f = 0 ; f < 4 ; f++)
-         {
-                  for(x = 0 ; x < 8 ; x++)
-                  {
-                           if((led[f + 8] & (1 << x)) != 0)
-                           {
-                                    digit |= (1 << ((f * 8) + x));
-                           }
-                  }
-         }
-         if(blink_type == 0)
-         {
-                  led_hex_write(0,digit);
-         }
-         else
-         {
-                  led_hex_write(circle,0);
-         }
-         delay_timer(blink_dim_time);
-         led_hex_write(circle,digit);
-         delay_timer(blink_shiny_time);
-}
-void yaz(void)
+void led_write(void)
 {
   unsigned char f;
   for(f = 0 ; f < 12 ; f++)
   {
-    spi( led[f] );
+    spi( led[(11 - f)] );
   }
-  latch();         
-}        
-         
+  latch();
+}        	
+// BELIRLI BIR ALANDAKI LEDLERI YAKAN FONKSIYON
+// x DEN BASLAYARAK y YE KADAR OLAN LEDLERI YAKAR
+// SAAT YADA DAKIKA YAZMAK ICIN UYGUNDUR
+void limit_yaz(unsigned char x,unsigned char y)
+{
+  unsigned char f;
+  // led_zero();
+  if( (x < 60) & (y < 60) )
+  {
+    if(y < x)
+    {
+      for(f = y ; f <= x ; f++)
+      {
+        led_yan(f);
+      }
+    }
+    else
+    {
+      for(f = x ; f <= y ; f++)
+      {
+        led_yan(f);
+      }
+    }
+  }
+}
+// BELIRLI BIR ALANDAKI LEDLERI SONDUREN FONSIYON 
+// ORNEGIN x DEN BASLAYARAK y YE KADAR SIL
+void limit_sil(unsigned char x,unsigned char y)
+{
+  unsigned char f;
+  // led_one();
+  if( (x < 60) & (y < 60) )
+  {
+    if(y < x)
+    {
+      for(f = y ; f <= x ; f++)
+      {
+        led_son(f);
+      }
+    }
+    else
+    {
+      for(f = x ; f <= y ; f++)
+      {
+        led_son(f);
+      }
+    }
+  }
+}
+// 0 - 60 between
+// SADECE BIR LEDI YAKAN FONKSIYON
+void led_yan(unsigned char xx)
+{
+  unsigned char x = xx;
+  if(xx < 61)
+  {
+    if(x == 0)
+    {
+      x = 60;
+    }
+    led[ (x - 1) / 8] |= (1 << ( (x - 1) % 8) );
+  }
+  // yaz();
+}
+// 0 - 60 between
+// SADECE BIR LEDI SONDUREN FONKSIYON
+void led_son(unsigned char xx)
+{
+  unsigned char x = xx;
+  if(x < 61)
+  {
+    if( x == 0)
+    {
+      x = 60;
+    }
+    led[ (x - 1) / 8] &= ~(1 << ( (x - 1) % 8) );
+  }
+  // yaz();
+}
+// BUTUN LEDLERI SONDUREN FONKSIYON
+void led_zero(void)
+{
+  unsigned char f = 60;
+  while(f)
+  {
+    led_son(f);
+    f--;
+  }
+}
+// BUTUN LEDLERI YAKAN FONKSIYON
+void led_one(void)
+{
+  unsigned char f = 60;
+  while(f)
+  {
+    led_yan(f);
+    f--;
+  }
+}
+
+void led_write_digit(unsigned char left,unsigned char right)
+{
+  switch(left)
+  {
+    case '.':
+    {
+      led[8] = 0x00;
+      led[9] = 0x00;
+    }
+    break;
+    case 0:
+    {
+      led[8] = 0xFF;
+      led[9] = 0x0F;
+    }
+    break;
+    case 1:
+    {
+      led[8] = 0x3C;
+      led[9] = 0x00;
+    }
+    break;
+    case 2:
+    {
+      led[8] = 0xCF;
+      led[9] = 0x33;
+    }
+    break;
+    case 3:
+    {
+      led[8] = 0xFF;
+      led[9] = 0x30;
+    }
+    break;
+    case 4:
+    {
+      led[8] = 0x3C;
+      led[9] = 0x3C;
+    }
+    break;
+    case 5:
+    {
+      led[8] = 0xF3;
+      led[9] = 0x3C;
+    }
+    break;
+    case 6:
+    {
+      led[8] = 0xF3;
+      led[9] = 0x3F;
+    }
+    break;
+    case 7:
+    {
+      led[8] = 0x3F;
+      led[9] = 0x00;
+    }
+    break;
+    case 8:
+    {
+      led[8] = 0xFF;
+      led[9] = 0x3F;
+    }
+    break;
+    case 9:
+    {
+      led[8] = 0xFF;
+      led[9] = 0x3C;
+    }
+    break;      
+    case 'A':
+    {
+      led[8] = 0x3F;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'B':
+    {
+      led[8] = 0xDB;
+      led[9] = 0x1F;
+    }
+    break;
+    case 'C':
+    {
+      led[8] = 0xC3;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'D':
+    {
+      led[8] = 0xDB;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'E':
+    {
+      led[8] = 0xC3;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'F':
+    {
+      led[8] = 0x03;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'G':
+    {
+      led[8] = 0xD3;
+      led[9] = 0x1F;
+    }
+    break;
+    case 'H':
+    {
+      led[8] = 0x3C;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'I':
+    {
+      led[8] = 0x00;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'J':
+    {
+      led[8] = 0xFC;
+      led[9] = 0x00;
+    }
+    break;
+    case 'K':
+    {
+      led[8] = 0x24;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'L':
+    {
+      led[8] = 0xC0;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'M':
+    {
+      led[8] = 0x34;
+      led[9] = 0x3B;
+    }
+    break;
+    case 'N':
+    {
+      led[8] = 0x3B;
+      led[9] = 0x07;
+    }
+    break;
+    case 'O':
+    {
+      led[8] = 0xDB;
+      led[9] = 0x06;
+    }
+    break;
+    case 'P':
+    {
+      led[8] = 0x0F;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'R':
+    {
+      led[8] = 0x03;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'S':
+    {
+      led[8] = 0xD1;
+      led[9] = 0x2C;
+    }
+    break;
+     case 'T':
+    {
+      led[8] = 0x00;
+      led[9] = 0x3F;
+    }
+    break;
+    case 'U':
+    {
+      led[8] = 0xFC;
+      led[9] = 0x0F;
+    }
+    break;
+    case 'V':
+    {
+      led[8] = 0xD4;
+      led[9] = 0x0A;
+    }
+    break;
+    case 'Y':
+    {
+      led[8] = 0xFD;
+      led[9] = 0x3D;
+    }
+    break;
+    case 'Z':
+    {
+      led[8] = 0xD8;
+      led[9] = 0x32;
+    }
+    break;
+    default:
+    {
+      led[8] = 0x00;
+      led[9] = 0x00;
+    }
+    break;    
+  }
+  switch(right)
+  {
+    case '.':
+    {
+      led[10] = 0x00;
+      led[11] = 0x00;
+    }
+    break;
+    case 0:
+    {
+      led[10] = 0xFF;
+      led[11] = 0x0F;
+    }
+    break;
+    case 1:
+    {
+      led[10] = 0x3C;
+      led[11] = 0x00;
+    }
+    break;
+    case 2:
+    {
+      led[10] = 0xCF;
+      led[11] = 0x33;
+    }
+    break;
+    case 3:
+    {
+      led[10] = 0xFF;
+      led[11] = 0x30;
+    }
+    break;
+    case 4:
+    {
+      led[10] = 0x3C;
+      led[11] = 0x3C;
+    }
+    break;
+    case 5:
+    {
+      led[10] = 0xF3;
+      led[11] = 0x3C;
+    }
+    break;
+    case 6:
+    {
+      led[10] = 0xF3;
+      led[11] = 0x3F;
+    }
+    break;
+    case 7:
+    {
+      led[10] = 0x3F;
+      led[11] = 0x00;
+    }
+    break;
+    case 8:
+    {
+      led[10] = 0xFF;
+      led[11] = 0x3F;
+    }
+    break;
+    case 9:
+    {
+      led[10] = 0xFF;
+      led[11] = 0x3C;
+    }
+    break;      
+
+    case 'A':
+    {
+      led[10] = 0x3F;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'B':
+    {
+      led[10] = 0xDB;
+      led[11] = 0x1F;
+    }
+    break;
+    case 'C':
+    {
+      led[10] = 0xC3;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'D':
+    {
+      led[10] = 0xDB;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'E':
+    {
+      led[10] = 0xC3;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'F':
+    {
+      led[10] = 0x03;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'G':
+    {
+      led[10] = 0xD3;
+      led[11] = 0x1F;
+    }
+    break;
+    case 'H':
+    {
+      led[10] = 0x3C;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'I':
+    {
+      led[10] = 0x00;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'J':
+    {
+      led[10] = 0xFC;
+      led[11] = 0x00;
+    }
+    break;
+    case 'K':
+    {
+      led[10] = 0x24;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'L':
+    {
+      led[10] = 0xC0;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'M':
+    {
+      led[10] = 0x34;
+      led[11] = 0x3B;
+    }
+    break;
+    case 'N':
+    {
+      led[10] = 0x3B;
+      led[11] = 0x07;
+    }
+    break;
+    case 'O':
+    {
+      led[10] = 0xDB;
+      led[11] = 0x06;
+    }
+    break;
+    case 'P':
+    {
+      led[10] = 0x0F;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'R':
+    {
+      led[10] = 0x03;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'S':
+    {
+      led[10] = 0xD1;
+      led[11] = 0x2C;
+    }
+    break;
+     case 'T':
+    {
+      led[10] = 0x00;
+      led[11] = 0x3F;
+    }
+    break;
+    case 'U':
+    {
+      led[10] = 0xFC;
+      led[11] = 0x0F;
+    }
+    break;
+    case 'V':
+    {
+      led[10] = 0xD4;
+      led[11] = 0x0A;
+    }
+    break;
+    case 'Y':
+    {
+      led[10] = 0xFD;
+      led[11] = 0x3D;
+    }
+    break;
+    case 'Z':
+    {
+      led[10] = 0xD8;
+      led[11] = 0x32;
+    }
+    break;
+    default:
+    {
+      led[10] = 0x00;
+      led[11] = 0x00;
+    }
+    break;    
+  }
+}
