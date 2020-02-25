f3018f65a8405205fc4209cef1cb729112ffab26
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-02-25 17:42:40 +0300

    Update yak.c

--- Software/lpc1114/yak.c
+++ Software/lpc1114/yak.c
@@ -1,23 +1,69 @@
-void pre_setting(void)
+void setting(void)
 {
-  
-}
-void setting(unsigned char set_number)
-{
-  unsigned char counter = 0;
+  unsigned char picky = SET_SECOND;
   while(1)
   {
-    button_repress_delay(); // maybe inside check_button ?
-    switch(check_button())
+   // button_repress_delay(); // maybe inside check_button ?
+    switch(picky)
     {
-      case setting_select_button_up:
+      case SET_SECOND:
         {
-          
+          picky = set_second();
         }
         break;
-      case setting_select_button_down:
+      case SET_MINUTE:
         {
-          
+          picky = set_minute();          
+        }
+        break;
+      case SET_HOUR:
+        {
+          picky = set_hour();
+        }
+        break;
+      case SET_WEEKDAY:
+        {
+          picky = set_weekday();          
+        }
+        break;
+      case SET_MONTHDAY:
+        {
+          picky = set_monthday();
+        }
+        break;
+      case SET_MONTH:
+        {
+          picky = set_month();          
+        }
+        break;
+      case SET_YEAR:
+        {
+          picky = set_year();
+        }
+        break;
+      case SET_ALR1_SECOND:
+        {
+          picky = set_alr1_second();          
+        }
+        break;
+      case SET_ALR1_MINUTE:
+        {
+          picky = set_alr1_minute();
+        }
+        break;
+      case SET_ALR1_HOUR:
+        {
+          picky = set_alr1_hour();          
+        }
+        break;
+      case SET_ALR2_MINUTE:
+        {
+          picky = set_alr2_minute();
+        }
+        break;
+      case SET_ALR2_HOUR:
+        {
+          picky = set_alr2_hour();          
         }
         break;
     }
