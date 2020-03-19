952ae676ffc00052c78e96dd2e413e9f4a5fc55f
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-19 10:07:04 +0300

    Update systick.c

--- Software/lpc1114/systick.c
+++ Software/lpc1114/systick.c
@@ -1,13 +1,14 @@
 #include "LPC11xx.h"
 #include "systick.h"
 
+// FOR 1 SECOND DELAY u_sec = SYSTEM_CLOCK_FREQUENCY * 1.000.000
 void delay_sys(unsigned int u_sec)
 {
   // systick max 24 bit === 0xFFFFFF
   if( (SYSTEM_CLOCK_FREQUENCY * u_sec) < 0xFFFFFF)
   {
     // systick start value
-    SysTick->LOAD = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 1; // must be tested "1" maybe too low "5" maybe
+    SysTick->LOAD = (SYSTEM_CLOCK_FREQUENCY * u_sec) - 5; // must be tested "1" maybe too low "5" maybe
   }
   else
   {
