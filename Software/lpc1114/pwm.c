075639ecc367a6cb7c6c16dd2176307ac4f3175a
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-19 14:10:22 +0300

    Update pwm.c

--- Software/lpc1114/pwm.c
+++ Software/lpc1114/pwm.c
@@ -1,3 +1,7 @@
+#include "LPC11xx.h"
+#include "e2prom.h"
+#include "error.h"
+
 void tmr16b1_pwm_init(void)
 {
   LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 16) | (1 << 8);          // ENABLE IOCON CLK AND TMR16B1 CLK
@@ -32,6 +36,27 @@ void brightness_down(void)
   LPC_TMR16B1->TCR = 0x01;                                    // ENABLE TIMER  
 }
 
+unsigned char led_power_init(void)
+{
+  unsigned char data[3];
+  LPC_TMR16B1->TCR = 0;                                       // DISABLE TIMER
+  LPC_TMR16B1->MR3 = 0;
+  if(e2prom_read(LED_LIGHT_SETTING_DUTY_LSB_ADDR,data,3) == ERR) // FIRST = LSB, SECOND = MSB, THIRD = PERCENT
+  {
+    return ERROR;
+  }
+  LPC_TMR16B1->MR3 = data[1];
+  LPC_TMR16B1->MR3 <<= 8;   //  LPC_TMR16B1->MR0 = (LPC_TMR16B1->MR0 << 8);
+  LPC_TMR16B1->MR3 |= data[0];
+  if(data[2] > 100)
+  {
+    data[2] = 100;
+  }
+  LPC_TMR16B1->MR0 = (LPC_TMR16B1->MR3 / 100 ) * data[2]; // data[2] max value must be 100
+  LPC_TMR16B1->TCR = 0x02;                                    // RESET TIMER
+  LPC_TMR16B1->TCR = 0x01;                                    // ENABLE TIMER    
+  return OK;
+}
 
 
 
