d8e91eb038dcfc65c510ce5d136514a23d50ec01
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-19 09:38:55 +0300

    Update button.c

--- Software/lpc1114/button.c
+++ Software/lpc1114/button.c
@@ -3,26 +3,7 @@
 #include "button.h"
 #include "delay.h"
 
-#define setting_alarm_button_pressed (LPC_GPIO1->DATA & (1 << 5))
-#define setting_clk_button_pressed (LPC_GPIO1->DATA & (1 << 11))
-#define stop_alarm_button_pressed (LPC_GPIO2->DATA & (1 << 4))
-#define sleep_button_pressed (LPC_GPIO2->DATA & (1 << 5))
-#define boot_button_pressed (LPC_GPIO0->DATA & (1 << 1))
-#define write_setting_button_pressed (LPC_GPIO3->DATA & (1 << 4))
-
-#define DELAY_FOR_BUTTON 0x0FFFFFFF
-#define SET_LRM 201
-#define SET_CLK 202
-#define STP_LRM 203
-#define SLP 204
-#define BOO 205
-#define WRT_SET 206
-#define LIG_DWN 207
-#define LIG_UP 208
-#define TM_DWN 209
-#define TM_UP 210
-
-void gpio_init(void)
+void button_init(void)
 {
 	//	ENABLE IOCON CLK
 	LPC_SYSCON->SYSAHBCLKCTRL		        |=	(1 << 16);
