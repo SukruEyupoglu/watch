34a75824dedd24f4723bbdb5c1bb5fc5defd3692
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-19 10:52:14 +0300

    Update systick.h

--- Software/lpc1114/systick.h
+++ Software/lpc1114/systick.h
@@ -12,9 +12,10 @@
 #define SYSTICK_TICKINT_BIT 1
 #define SYSTICK_ENABLE_BIT 0
 
+#define WAITING_FOR_ACTION 1
+#define ONLY_DELAY 2
 
-
-
+void deadline_sec(unsigned char wait_for_dead);
 void delay_sys(unsigned int u_sec);
 void SysTick_Handler(void);
 
