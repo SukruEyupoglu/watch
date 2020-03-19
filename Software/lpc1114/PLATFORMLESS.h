fe5302e7c4cccf02ece46c4d66f17bf9daebad78
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-19 14:33:12 +0300

    Update PLATFORMLESS.h

--- Software/lpc1114/PLATFORMLESS.h
+++ Software/lpc1114/PLATFORMLESS.h
@@ -1,6 +1,14 @@
 
 
 
+
+void int_to_char(unsigned int data,unsigned char * part0,unsigned char * part1,unsigned char * part2,unsigned char * part3);
+void char_to_int(unsigned int * data,unsigned char part0,unsigned char part1,unsigned char part2,unsigned char part3);
+void short_to_char(unsigned short data,unsigned char * part0,unsigned char * part1);
+void char_to_short(unsigned short * data,unsigned char part0,unsigned char part1);
+void longlong_to_int(unsigned long long data,unsigned int * part0,unsigned int * part1);
+void int_to_longlong(unsigned long long * data,unsigned int part0,unsigned int part1);
+
 void delay(volatile unsigned int bekle);
 
 unsigned char check_hex_data(unsigned char data);
