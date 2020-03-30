af4284a78453207187ccd5b4604d9c8f2c2aa55b
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-03-30 11:17:15 +0300

    Update button.c

--- Software/lpc1114/button.c
+++ Software/lpc1114/button.c
@@ -1,39 +1,53 @@
 
 #include "LPC11xx.h"
-#include "button.h"
-#include "delay.h"
+#include "lpc1114_button.h"
 
-void button_init(void)
+
+void gpio_init(void)
 {
 	//	ENABLE IOCON CLK
-	LPC_SYSCON->SYSAHBCLKCTRL		        |=	(1 << 16);
+	LPC_SYSCON->SYSAHBCLKCTRL		|=	(1 << 16);
 	//	ENABLE GPIO CLK
-	LPC_SYSCON->SYSAHBCLKCTRL			|=	(1 << 6);
-	//	IOCONFIG SPI SETTINGS
-	LPC_IOCON->PIO1_5		            	=	0x0; //setting alarm
-	LPC_IOCON->PIO1_11	            		=	0x0; //setting clk
-	LPC_IOCON->PIO2_4       	        	=	0x0; //stop alarm
-	LPC_IOCON->PIO2_5 		          	=	0x0; //sleep
-	LPC_IOCON->PIO0_1       	       		=	0x0; //boot
-	LPC_IOCON->PIO3_4 		          	=	0x0; //write setting
-    	LPC_GPIO1->DIR					&=	~(1 << 5);
-    	LPC_GPIO1->DIR					&=	~(1 << 11);
-    	LPC_GPIO2->DIR					&=	~(1 << 4);
-    	LPC_GPIO2->DIR					&=	~(1 << 5);
-    	LPC_GPIO0->DIR					&=	~(1 << 1);
-    	LPC_GPIO3->DIR					&=	~(1 << 4);
+	LPC_SYSCON->SYSAHBCLKCTRL		|=	(1 << 6);
+	//	IOCONFIG gpio SETTINGS
+	LPC_IOCON->PIO1_5		        =	0x0; //setting alarm
+	LPC_IOCON->PIO1_11	            	=	0x0; //setting clk
+	LPC_IOCON->PIO2_4       	        =	0x0; //stop alarm
+	LPC_IOCON->PIO2_5 		        =	0x0; //sleep
+	LPC_IOCON->PIO0_1       	       	=	0x0; //boot
+	LPC_IOCON->PIO3_4 		        =	0x0; //write setting
+  // make direction input button pins
+  LPC_GPIO1->DIR				&=	~(1 << 5);
+  LPC_GPIO1->DIR				&=	~(1 << 11);
+  LPC_GPIO2->DIR				&=	~(1 << 4);
+  LPC_GPIO2->DIR				&=	~(1 << 5);
+  LPC_GPIO0->DIR				&=	~(1 << 1);
+  LPC_GPIO3->DIR				&=	~(1 << 4);
 }
 
 unsigned char check_button(void)
 {
-  delay(DELAY_FOR_BUTTON);
   if(setting_alarm_button_pressed != 0)
   {
-    return SET_LRM;
+	if(boot_button_pressed != 0)
+  	{
+    		return SETTING_LRM;
+  	}
+	else
+	{
+		return SET_LRM;
+	}
   }
   if(setting_clk_button_pressed != 0)
   {
-    return SET_CLK;
+	if(boot_button_pressed != 0)
+  	{
+    		return SETTING_CLK;
+  	}
+	else
+	{
+    		return SET_CLK;
+	}
   }
   if(stop_alarm_button_pressed != 0)
   {
@@ -51,8 +65,6 @@ unsigned char check_button(void)
   {
     return WRT_SET;
   }
-return 0;
-}
 /*
   switch (adc_2_light_up_down_read())
   {
@@ -76,12 +88,6 @@ return 0;
       return TM_UP;
     }
   }
-  
+  */
 return 0;
 }
-
-*/
-
-
-
-
