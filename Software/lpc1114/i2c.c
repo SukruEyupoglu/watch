db86ed806d46c8e3938f5f93eae463ed248fcef4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-09 00:12:00 +0300

    Update i2c.c

--- Software/lpc1114/i2c.c
+++ Software/lpc1114/i2c.c
@@ -106,7 +106,7 @@ unsigned char i2c
 		while(CHECK_SI_BIT);
 		if(LPC_I2C->STAT == I2CSTAT_ACK_0x40)
 		{
-			//LPC_I2C->CONSET = I2CONSET_AA_BIT2;
+			LPC_I2C->CONSET = I2CONSET_AA_BIT2;
 			CLEAR_SI_BIT;
 		}
 		else
@@ -121,7 +121,7 @@ unsigned char i2c
 			if(LPC_I2C->STAT == I2CSTAT_ACK_0x50)
 			{
 				*(data + f) = LPC_I2C->DAT;
-				if(data_size < 2)
+				if(f == (data_size -1) )
 				{
 					LPC_I2C->CONCLR = I2CONCLR_AA_BIT2;
 				}
