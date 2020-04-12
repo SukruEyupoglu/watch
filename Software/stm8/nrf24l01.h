8984c36e92e57881a5696d0608dfd0fac19d4188
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-12 13:23:31 +0300

    Update nrf24l01.h

--- Software/stm8/nrf24l01.h
+++ Software/stm8/nrf24l01.h
@@ -28,12 +28,25 @@
 #define SETUP_AW_ADDR_BYTE_4        0x10
 #define SETUP_AW_ADDR_BYTE_5        0x11
 
+#define SETUP_RETR 0x04
+#define SETUP_RETR_ARD              0xF0
+#define SETUP_RETR_ARC              0x0F
 
+#define RF_CH 0x05
+#define RF_CH_RF_CH                 0x7F
 
+#define RF_SETUP 0x06
+#define RF_SETUP_CONT_WAVE          (1 << 7)
+#define RF_SETUP_PLL_LOCK           (1 << 4)
+// DATA RATE SELECT - SPEED SELECT
+#define RF_SETUP_RF_250K            (1 << 5)  // if used not look at to 1M_2M
+#define RF_SETUP_RF_1M_2M           (1 << 3)  // 1 = 2M --- 0 = 1M if 250K not used
+// DBM SELECT -OUTPUT POWER SELECT - DISTANCE SELECT
+#define RF_SETUP_RF_PWR_DBM_18      0xF9
+#define RF_SETUP_RF_PWR_DBM_12      0xFB
+#define RF_SETUP_RF_PWR_DBM_6       0xFD
+#define RF_SETUP_RF_PWR_DBM_0       0xFF
 
-
-
-
-
-
+#define STATUS 0x07
+// underconstruction
 
