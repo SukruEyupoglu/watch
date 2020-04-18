472e500c77fc5cfb1861c3a1ce761fd010c38f8d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-18 19:17:53 +0300

    Update adc.h

--- Software/lpc1114/adc.h
+++ Software/lpc1114/adc.h
@@ -21,14 +21,27 @@ unsigned char adc_4_pir_read(void);
 #define AD0CR_START_CONVERSION          (1 << 24)
 // if capture or match u need for start look datasheet
 #define AD0CR_START_CONVERSION_WITH_CAP0_MAT0          (0x7 << 24)
-
 #define AD0CR_START_EDGE                (1 << 27)
 
+#define AD0GDR_V_VREF                   (0x3FF << 6)
+#define AD0GDR_CHN                      (0x7 << 24)
+#define AD0GDR_OVERRUN                  (1 << 30)
+#define AD0GDR_DONE                     (1 << 31)
 
+#define AD0INTEN_ADINTEN_AD0            (1 << 0)
+#define AD0INTEN_ADINTEN_AD1            (1 << 1)
+#define AD0INTEN_ADINTEN_AD2            (1 << 2)
+#define AD0INTEN_ADINTEN_AD3            (1 << 3)
+#define AD0INTEN_ADINTEN_AD4            (1 << 4)
+#define AD0INTEN_ADINTEN_AD5            (1 << 5)
+#define AD0INTEN_ADINTEN_AD6            (1 << 6)
+#define AD0INTEN_ADINTEN_AD7            (1 << 7)
 
+#define AD0INTEN_ADGINTEN               (1 << 8)
 
-
-
+#define AD0DRX_V_VREF                   (0x3FF << 6)
+#define AD0DRX_OVERRUN                  (1 << 30)
+#define AD0DRX_DONE                     (1 << 31)
 
 
 
