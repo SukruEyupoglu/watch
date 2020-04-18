dc5181cf0d6bd8214c6efe44a5377fb11c11b159
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-18 21:32:18 +0300

    Update adc.c

--- Software/lpc1114/adc.c
+++ Software/lpc1114/adc.c
@@ -1,4 +1,5 @@
 #include "LPC11xx.h"
+#include "adc.h"
 
 unsigned char adc_read(unsigned char adcx)
 {
@@ -36,71 +37,17 @@ void adc_init(void)
 }
 unsigned char adc_0_mic_read(void)
 {
-		return 0;
+	return adc_read(ADC0);
 }
 unsigned char adc_1_time_up_down_read(void)
 {
-	unsigned int x;
-	// START CONVERSION
-	LPC_ADC->CR					|=	(1 << 24);
-	while((LPC_ADC->DR[0] < 0x7FFFFFFF));
-	{
-		x = LPC_ADC->DR[1];
-		if(x & (1 << 31))
-		   {
-			   // STOP CONVERSION
-			   LPC_ADC->CR	&=	~(1 << 24);
-			   x >>= 6;
-			   x  &= 0x3FF;
-			   // 0x3FF = 1023 and %60 is up %30 is down
-			   // between %70 and %50 is up
-			   // between %40 and %20 is down
-			   // %70 = 716 --> %50 = 512 --> %40 = 409 --> %20 = 204
-			   if((x < 716) & (x > 512))
-			   {
-				   return 20;
-			   }
-			   if((x < 409) & (x > 204))
-			   {
-				   return 5;
-			   }
-			break;
-		   }
-	}
-	return 0;
+	return adc_read(ADC1);
 }
 unsigned char adc_2_light_up_down_read(void)
 {
-	unsigned int x;
-	// START CONVERSION
-	LPC_ADC->CR					|=	(1 << 24);
-	while(1)
-	{
-		x = LPC_ADC->DR[2];
-		if(x & (1 << 31))
-		   {
-			   // STOP CONVERSION
-			   LPC_ADC->CR	&=	~(1 << 24);
-			   x >>= 6;
-			   x  &= 0x3FF;
-			   // 0x3FF = 1023 and %60 is up %30 is down
-			   // between %70 and %50 is up
-			   // between %40 and %20 is down
-			   // %70 = 716 --> %50 = 512 --> %40 = 409 --> %20 = 204
-			   if((x < 716) & (x > 512))
-			   {
-				   return 20;
-			   }
-			   if((x < 409) & (x > 204))
-			   {
-				   return 5;
-			   }
-			break;
-		   }
-	}
-	return 0;		
+	return adc_read(ADC2);
 }
 unsigned char adc_4_pir_read(void)
 {
-		return 0;
+	return adc_read(ADC4);
 }
