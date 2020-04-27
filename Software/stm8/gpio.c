8c246d4ca145b8af7bf9381c4198ee7c35679b3f
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-27 22:25:18 +0300

    Update gpio.c

--- Software/stm8/gpio.c
+++ Software/stm8/gpio.c
@@ -28,5 +28,34 @@ void PC3_interrupt_enable(void)
   PC_CR2 |= (1 << 3);           // INTERRUPT ENABLE PC3
 }
 
-
+void nrf_gpio_init(void)
+{
+  //	NRF IRQ SETTINGS
+	//	MAKE DIRECTION INPUT PC4,PC3
+	PC_DDR &= ~( (1 << 3) | (1 << 4) ); // 3 FOR NRF1 --- 4 FOR NRF2
+  
+	//	NRF CE SETTINGS CHIP ENABLE ACTIVES RX - TX MODE --- ACTIVE HIGH 
+	//	RX MODE CE MUST BE HIGH 
+	//	TX MODE CE MUST BE A PULSE MORE THAN 10us -- 4ms DEADLINE FOR CE HIGH TIME
+	//	YOU CAN USE 15us FOR COMMINICATION BEWARE FOR TX MODE DEADLINE
+	PA_DDR |= (1 << 3); // NRF1
+	PD_DDR |= (1 << 3); // NRF2
+	PA_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
+	PD_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
+	PA_ODR &= ~(1 << 3); // OUTPUT LOW
+	PD_ODR &= ~(1 << 3); // OUTPUT LOW
+  
+	//	NRF CSN SETTINGS SPI CHIP SELECT ACTIVE LOW
+	PA_DDR |= (1 << 2); // NRF1
+	PD_DDR |= (1 << 2); // NRF2
+	PA_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
+	PD_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
+	PA_ODR |= (1 << 3); // OUTPUT HIGH
+	PD_ODR |= (1 << 3); // OUTPUT HIGH
+	
+ 	//	100ms delay for nrf24l01 wakeup ---  FROM DATASHEET
+	// delay(100000);
+  // For double device 
+        
+}
 
