5a4dd9b5fc7641f868c442e84354feb282a77ae6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-27 22:28:51 +0300

    Update nrf24l01.c

--- Software/stm8/nrf24l01.c
+++ Software/stm8/nrf24l01.c
@@ -3,34 +3,6 @@
 #include "stm8s.h"
 #include "spi.h"
 
-void nrf_gpio_init(void)
-{
-  	//	NRF IRQ SETTINGS
-	//	MAKE DIRECTION INPUT PC4,PC3
-	PC_DDR &= ~( (1 << 3) | (1 << 4) ); // 3 FOR NRF1 --- 4 FOR NRF2
-	//	NRF CE SETTINGS CHIP ENABLE ACTIVES RX - TX MODE --- ACTIVE HIGH 
-	//	RX MODE CE MUST BE HIGH 
-	//	TX MODE CE MUST BE A PULSE MORE THAN 10us -- 4ms DEADLINE FOR CE HIGH TIME
-	//	YOU CAN USE 15us FOR COMMINICATION BEWARE FOR TX MODE DEADLINE
-	PA_DDR |= (1 << 3); // NRF1
-	PD_DDR |= (1 << 3); // NRF2
-	PA_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
-	PD_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
-	PA_ODR &= ~(1 << 3); // OUTPUT LOW
-	PD_ODR &= ~(1 << 3); // OUTPUT LOW
-	//	NRF CSN SETTINGS SPI CHIP SELECT ACTIVE LOW
-	PA_DDR |= (1 << 2); // NRF1
-	PD_DDR |= (1 << 2); // NRF2
-	PA_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1
-	PD_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2
-	PA_ODR |= (1 << 3); // OUTPUT HIGH
-	PD_ODR |= (1 << 3); // OUTPUT HIGH
-	
- 	//	100ms delay for nrf24l01 wakeup ---  FROM DATASHEET
-	// delay(100000);
-        // For double device 
-        
-}
 // ready for testing only 1 byte data waiting for rx look at DYNPD later
 void nrf24l01_init(void)
 {
@@ -112,7 +84,7 @@ void set_rx_pw_px(unsigned char pipe,unsigned char x_1_32) // x_1_32 = Number of
 {
 	NRF_write_reg(W_REGISTER | (RX_PW_PX + pipe) , x_1_32);
 }
-
+/*
 void NRF_TX_INIT(void)
 {
 	// config registerini set et default tx modu zaten 1.5ms bekle
@@ -188,6 +160,7 @@ void NRF_RX_INIT_NO_ACK(void)
 	// ack'li veri tranferi aktif etme ayari
 	NRF_write_reg(W_REGISTER | FEATURE , (1 << 0));
 }
+*/
 void NRF_write_buf(unsigned char * data,unsigned char size)
 {
     unsigned char f;
@@ -255,12 +228,12 @@ void NRF_flush_rx(void) {
     spi(FLUSH_RX);
     NRF_CSN_HIGH;
 }
-
+/*
 //	sadece ilk bytini degistir gonder toplamda 5 byte var
 void NRF_send(unsigned char * data , unsigned char size)
 {
-	unsigned char var[5] = {data,0x1,0x2,0x3,0x4};
-	NRF_write_buf(W_TX_PAYLOAD, var, 5);
+    unsigned char var[5] = {data,0x1,0x2,0x3,0x4};
+    NRF_write_buf(W_TX_PAYLOAD, var, 5);
     NRF_CE_HIGH;
     delay(10);
     NRF_CE_LOW;
@@ -273,6 +246,6 @@ unsigned char NRF_get(void)
 	NRF_read_buf(R_RX_PAYLOAD, var, 5);
 	return var[0];
 }
-
+*/
 
 
