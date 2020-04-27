fefd77470219e21b5c919725b73d6075f8f4aaaa
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-27 19:07:25 +0300

    Create delay.h

---
+++ Software/stm8/delay.h
@@ -0,0 +1,11 @@
+
+
+#define delay_10us delay(20)
+
+void delay_us(volatile unsigned char bekle);
+void delay_ms(volatile unsigned char bekle);
+
+////////////////////////////////////////////////////////////////
+// THIS TESTED WITH OSCILOSKOP
+  // SET CLK TO FULL SPEED (16MHZ)
+//CLK_CKDIVR = 0;
