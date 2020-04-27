6b3ff4e40605f62de34becf13f7dc19330a09db4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-04-27 18:47:06 +0300

    Create delay.c

---
+++ Software/stm8/delay.c
@@ -0,0 +1,40 @@
+
+
+
+////////////////////////////////////////////////////////////////
+// THIS TESTED WITH OSCILOSKOP
+  // SET CLK TO FULL SPEED (16MHZ)
+  //CLK_CKDIVR = 0;
+void delay_us(volatile unsigned char bekle);
+void delay_us(volatile unsigned char bekle)
+{
+	while(bekle)
+	{
+		bekle--;
+	}
+	//	delay(0)	1.63us
+	//	delay(1)	2.07us
+	//	delay(2)	2.51us
+	//	delay(3)	2.95us
+	//	delay(4)	3.39us
+	//	delay(5)	3.83us
+	//	delay(6)	4.27us
+	//	delay(19)	9.99us
+	//	delay(20)	10.43us
+	//	delay(224)	100.1us	
+	//	delay(255)	113.8us  //MAX
+}
+#define delay_10us delay(20)
+///////////////////////////////////////////////////////////////////////
+void delay_ms(volatile unsigned char bekle);
+void delay_ms(volatile unsigned char bekle)
+{
+	while(bekle)
+	{
+		bekle--;
+		delay_us(224);delay_us(224);delay_us(224);delay_us(224);delay_us(224);
+		delay_us(224);delay_us(224);delay_us(224);delay_us(224);delay_us(224);
+	}
+}
+
+
