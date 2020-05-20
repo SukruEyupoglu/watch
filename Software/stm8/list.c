8a0315ffa8fb7a02ec977a5c38fc063c06c13000
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-05-20 16:10:25 +0300

    Update list.c

--- Software/stm8/list.c
+++ Software/stm8/list.c
@@ -1,8 +1,27 @@
-#define SPACE 0x20
+
+
+#include "list.h"
+
 void list(void)
 {
-  const unsigned char status[15] = {'S','T','A','T','U','S',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,':'};
-  const unsigned char rx_addr_p0_1[15] = {'R','X','_','A','D','D','R','_','P','0','-','1',SPACE,SPACE,':'};
-  const unsigned char rx_addr[15] = {'R','X','_','A','D','D','R','_','P','2','-','5',SPACE,SPACE,':'};
+  const unsigned char status[15] = {'S','T','A','T','U','S',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char rx_addr_p0_1[15] = {'R','X','_','A','D','D','R','_','P','0','-','1',SPACE,SPACE,'='};
+  const unsigned char rx_addr[15] = {'R','X','_','A','D','D','R','_','P','2','-','5',SPACE,SPACE,'='};
+  const unsigned char tx_addr[15] = {'T','X','_','A','D','D','R',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char rx_pw_p0_6[15] = {'R','X','_','P','W','_','P','0','-','6',SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char en_aa[15] = {'E','N','_','A','A',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char en_rxaddr[15] = {'E','N','_','R','X','A','D','D','R',SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char rf_ch[15] = {'R','F','_','C','H',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char rf_setup[15] = {'R','F','_','S','E','T','U','P',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char config[15] = {'C','O','N','F','I','G',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,'='};
+  const unsigned char dynpd_feature[15] = {'D','Y','N','P','D','/','F','E','A','T','U','R','E',SPACE,'='};
+  
+  
   
 }
+
+
+
+
+
+
