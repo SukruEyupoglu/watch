7edaebf96bb115df05a0f528763eca530cdec8ce
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-05-20 15:06:22 +0300

    Create list.c

---
+++ Software/stm8/list.c
@@ -0,0 +1,8 @@
+#define SPACE 0x20
+void list(void)
+{
+  const unsigned char status[15] = {'S','T','A','T','U','S',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,':'};
+  const unsigned char status[15] = {'R','X','_','A','D','D','R','_','P','0','-','1',SPACE,SPACE,':'};
+  const unsigned char status[15] = {'R','X','_','A','D','D','R','_','P','2','-','5',SPACE,SPACE,':'};
+  
+}
