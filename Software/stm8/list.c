b68decbb005849b8dd219388bd10355e1a618f56
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-05-20 15:21:00 +0300

    Update list.c

--- Software/stm8/list.c
+++ Software/stm8/list.c
@@ -2,7 +2,7 @@
 void list(void)
 {
   const unsigned char status[15] = {'S','T','A','T','U','S',SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,SPACE,':'};
-  const unsigned char status[15] = {'R','X','_','A','D','D','R','_','P','0','-','1',SPACE,SPACE,':'};
-  const unsigned char status[15] = {'R','X','_','A','D','D','R','_','P','2','-','5',SPACE,SPACE,':'};
+  const unsigned char rx_addr_p0_1[15] = {'R','X','_','A','D','D','R','_','P','0','-','1',SPACE,SPACE,':'};
+  const unsigned char rx_addr[15] = {'R','X','_','A','D','D','R','_','P','2','-','5',SPACE,SPACE,':'};
   
 }
