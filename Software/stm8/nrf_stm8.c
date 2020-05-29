a99d9e01753b50829f02dae0e44cf908271fd974
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-05-29 21:40:29 +0300

    Update nrf_stm8.c

--- Software/stm8/nrf_stm8.c
+++ Software/stm8/nrf_stm8.c
@@ -1,8 +1,11 @@
 #include "stm8s.h"
-
+#include "list.h"
+#include "nrf24l01.h"
 
 int main(void)
 {
+  nrf24l01_init(); // default tx
+  second_nrf24l01_init(); // default tx
   while(1)
   {
     
