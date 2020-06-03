3f9e6547d0899fbe6083c428f969d7d2f642c877
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-03 22:55:40 +0300

    Update nrf_stm8.c

--- Software/stm8/nrf_stm8.c
+++ Software/stm8/nrf_stm8.c
@@ -4,8 +4,20 @@
 
 int main(void)
 {
+  unsigned char send_data[5] = {5,4,3,2,1};
+  unsigned char get_data[5];
   nrf24l01_init(); // default tx
-  second_nrf24l01_init(); // default tx
+  second_nrf24l01_init(); // default rx
+  
+  nrf_send(send_data , 5);
+  wait_for_second_nrf();
+  nrf_get(get_data , 5);
+  
+  if(send_data[0] == get_data[0])
+  {
+    led_yak();
+  }
+  
   while(1)
   {
     
