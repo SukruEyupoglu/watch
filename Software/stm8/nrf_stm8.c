9db37576d5a93cc7ef7776710037f285c00a7e9c
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-03 23:35:38 +0300

    Update nrf_stm8.c

--- Software/stm8/nrf_stm8.c
+++ Software/stm8/nrf_stm8.c
@@ -1,13 +1,16 @@
 #include "stm8s.h"
 #include "list.h"
 #include "nrf24l01.h"
+#include "led.h"
 
 int main(void)
 {
   unsigned char send_data[5] = {5,4,3,2,1};
   unsigned char get_data[5];
+  
   nrf24l01_init(); // default tx
   second_nrf24l01_init(); // default rx
+  led_init();
   
   nrf_send(send_data , 5);
   wait_for_second_nrf(); // BEWARE multi_nrf_select = 2; from here
@@ -15,7 +18,7 @@ int main(void)
   
   if(send_data[0] == get_data[0])
   {
-    led_yak();
+    led_open();
   }
   
   while(1)
