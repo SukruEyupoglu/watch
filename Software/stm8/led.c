ec6fe3e2f38ef4e0d4706257e1b52ee421d4d7ac
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-03 23:34:05 +0300

    Update led.c

--- Software/stm8/led.c
+++ Software/stm8/led.c
@@ -1,15 +1,18 @@
-void led_init()
+#include "stm8s.h"
+#include "led.h"
+
+void led_init(void)
 {
   PB_DDR |= (1 << 5); // DEFAULT OPEN DRAIN
   PB_ODR |= (1 << 5); // OUTPUT HIGH AND LED DIMMED
 }
 
-void led_open()
+void led_open(void)
 {
   PD_ODR &= ~(1 << 5); // OUTPUT LOW AND OPEN DRAIN AND LED SHINING
 }
 
-void led_close()
+void led_close(void)
 {
   PB_ODR |= (1 << 5); // OUTPUT HIGH AND LED DIMMED
 }
