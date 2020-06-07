d44d3d76a651e7a45baf0fcf17730ee3d23fe520
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-07 20:22:21 +0300

    Update nrf.c

--- Software/stm8/nrf.c
+++ Software/stm8/nrf.c
@@ -3,123 +3,9 @@
 #include "stm8s.h"
 #include "spi.h"
 
-volatile unsigned char multi_nrf_select = 1;
-
-void nrf_csn_low(void)
-{
-	if(multi_nrf_select == 1)
-	{
-		PA_ODR &= ~(1 << 2); // MAKE A2 LOW
-	}
-	else
-	{
-		PD_ODR &= ~(1 << 2); // MAKE D2 LOW
-	}
-}
-
-void nrf_csn_high(void)
-{
-	if(multi_nrf_select == 1)
-	{
-		PA_ODR |= (1 << 2); // MAKE A2 HIGH
-	}
-	else
-	{
-		PD_ODR |= (1 << 2); // MAKE D2 HIGH
-	}
-}
-
-void nrf_ce_low(void)
-{
-	if(multi_nrf_select == 1)
-	{
-		PA_ODR &= ~(1 << 3); // MAKE A3 LOW
-	}
-	else
-	{
-		PD_ODR &= ~(1 << 3); // MAKE D3 LOW
-	}
-}
-
-void nrf_ce_high(void)
-{
-	if(multi_nrf_select == 1)
-	{
-		PA_ODR |= (1 << 3); // MAKE A3 HIGH
-	}
-	else
-	{
-		PD_ODR |= (1 << 3); // MAKE D3 HIGH
-	}
-}
-
-
-void nrf_send(unsigned char * data , unsigned char size)
-{
-    nrf_write_buf(data , size);
-    NRF_CE_HIGH;
-    delay_10us;
-    NRF_CE_LOW;
-}
-	  
-
-void nrf_get(unsigned char * data , unsigned char size)
-{
-    nrf_read_buf(data , size);
-}
-
-// if IRQ be LOW call this function for reason
-unsigned char check_irq_status(void)
-{
-  switch(nrf_read_reg(STATUS_REG) & 
-    (
-      STATUS_RX_DR |
-      STATUS_TX_DS |
-      STATUS_MAX_RT
-    ) )
-  {
-    case STATUS_RX_DR :
-    {
-      return RX_DR_INT;
-    }
-    case STATUS_TX_DS :
-    {
-      return TX_DS_INT;
-    }
-    case STATUS_MAX_RT :
-    {
-      return MAX_RT_INT;
-    }
-  }
-  return VOID_INT;
-}
-
-// second nrf init same ic
-void second_nrf24l01_init(void)
-{
-	multi_nrf_select = 2;
-	nrf24l01_init();
-	make_rx();
-	multi_nrf_select = 1;
-}
-
-void wait_for_second_nrf(void) // BEWARE multi_nrf_select = 2; from here
-{
-	unsigned char x;
-	multi_nrf_select = 2;
-	while(1)
-	{
-		x = nrf_read_reg(STATUS_REG);
-		if( x & STATUS_RX_DR)
-		{
-			break;
-		}
-	}
-}
-
 
 // ready for testing only 1 byte data waiting for rx look at DYNPD later
-void nrf24l01_init(void)
+void nrf24l01_init(unsigned char which_nrf)
 {
 	delay_ms(100);							// wait until it happens to power down mode 100ms
 	nrf_write_reg(W_REGISTER | CONFIG , CONFIG_PWR_UP);		// only start up for make standby-1
@@ -148,134 +34,18 @@ void nrf24l01_init(void)
 	// delay_us(130);							// wait for convert to tx or rx from datasheet
 	// no need to hange comminication address for now
 }
-/*
-void nrf24l01_init_from_eeprom(void)
-{
-	unsigned char data;
-	unsigned char data_array[5];
-	delay_ms(100);							// wait until it happens to power down mode 100ms
-	
-	// Configuration Register
-	eeprom_read(CONFIG_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | CONFIG , data);			// only start up for make standby-1
-	
-	delay_ms(2);							// wait 1.5ms for power up
-	
-	// Enable ‘Auto Acknowledgment’ Function Disable this functionality to be compatible with nRF2401
-	eeprom_read(EN_AA_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | EN_AA , data);
-	
-	// Enabled RX Addresses
-	eeprom_read(EN_RXADDR_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | EN_RXADDR , data);
-	
-	// Setup of Address Widths (common for all data pipes)
-	eeprom_read(SETUP_AW_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | SETUP_AW , data);
-	
-	// Setup of Automatic Retransmission
-	eeprom_read(SETUP_RETR_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | SETUP_RETR , data);
-	
-	// RF Channel 0 - 127
-	eeprom_read(RF_CH_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RF_CH , data);
-	
-	// RF Setup Register
-	eeprom_read(RF_SETUP_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RF_SETUP , data);
-	
-	// Receive address data pipe (LSByte is written first)
-	eeprom_read(RX_ADDR_P0_EE_ADDR, data_array, MAX_ADDR_SIZE);
-	set_rx_addr_p_0_1(P0,data_array);
-	eeprom_read(RX_ADDR_P1_EE_ADDR, data_array, MAX_ADDR_SIZE);
-	set_rx_addr_p_0_1(P1,data_array);
-	eeprom_read(RX_ADDR_P2_EE_ADDR, &data, 1);
-	set_rx_addr_p_2_3_4_5(P2,data);
-	eeprom_read(RX_ADDR_P3_EE_ADDR, &data, 1);
-	set_rx_addr_p_2_3_4_5(P3,data);	
-	eeprom_read(RX_ADDR_P4_EE_ADDR, &data, 1);
-	set_rx_addr_p_2_3_4_5(P4,data);
-	eeprom_read(RX_ADDR_P5_EE_ADDR, &data, 1);
-	set_rx_addr_p_2_3_4_5(P5,data);
-	
-	// Transmit address. Used for a PTX device only. (LSByte is written first)
-	eeprom_read(TX_ADDR_EE_ADDR, data_array, MAX_ADDR_SIZE);
-	set_tx_addr(data_array);
-	
-	// Number of waiting bytes in RX payload without DYNPD in data pipe ( 1 to 32 bytes )
-	eeprom_read(RX_PW_P0_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P0 , data);
-	eeprom_read(RX_PW_P1_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P1 , data);
-	eeprom_read(RX_PW_P2_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P2 , data);
-	eeprom_read(RX_PW_P3_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P3 , data);
-	eeprom_read(RX_PW_P4_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P4 , data);
-	eeprom_read(RX_PW_P5_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | RX_PW_P5 , data);
-
-	// Enable dynamic payload length
-	eeprom_read(DYNPD_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | DYNPD , data);
-	
-	// Enable dynamic payload length
-	eeprom_read(FEATURE_EE_ADDR, &data, 1);
-	nrf_write_reg(W_REGISTER | FEATURE , data);
-	
-	// default is tx at config register
-	make_tx();
-	// make_rx();
-	// wait for convert to tx or rx from datasheet
-	// delay_us(130);
-}
-*/
-void make_tx(void)
+void make_tx(unsigned char which_nrf)
 {
         NRF_CE_LOW;
 	nrf_write_reg(W_REGISTER | CONFIG , CONFIG_PWR_UP | CONFIG_MASK_TX_DS);
         delay_us(130);
 }
 
-void make_rx(void)
+void make_rx(unsigned char which_nrf)
 {
 	nrf_write_reg(W_REGISTER | CONFIG , (CONFIG_PWR_UP | CONFIG_PRIM_RX | CONFIG_MASK_RX_DR) );
         delay_us(130);
         NRF_CE_HIGH;
-}
-
-void get_rx_addr_p_0_1(unsigned char x_0_1,unsigned char * addr) // 5 byte total
-{
-
-    unsigned char f;
-    NRF_CSN_LOW;
-    spi(R_REGISTER | (RX_ADDR_PX + x_0_1));
-    for (f = 0 ; f < 5 ; f++)
-    {
-    	* (addr + f) = spi(NOP); // LSByte is read first
-    }
-    NRF_CSN_HIGH;
-}
-
-// MSB 4 BYTE SAME AS P1 ONLY LAST BYTE WRITE THIS REG (FROM DATASHEET)
-unsigned char get_rx_addr_p_2_3_4_5(unsigned char x_2_3_4_5)
-{
-	return nrf_read_reg(R_REGISTER | (RX_ADDR_PX + x_2_3_4_5) );
-}
-
-void get_tx_addr(unsigned char * addr)
-{
-
-    unsigned char f;
-    NRF_CSN_LOW;
-    spi(R_REGISTER | TX_ADDR);
-    for (f = 0 ; f < 5 ; f++)
-    {
-    	*( addr + f) = spi(NOP); // LSByte is read first
-    }
-    NRF_CSN_HIGH;
 }
 
 // set nrf rx comminication addr
