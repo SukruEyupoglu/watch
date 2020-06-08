5e1bf4b7f78e87c691ac1ede190a922c93bb4cdc
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-08 20:07:49 +0300

    Update nrf.c

--- Software/stm8/nrf.c
+++ Software/stm8/nrf.c
@@ -3,6 +3,41 @@
 #include "stm8s.h"
 #include "spi.h"
 
+void nrf_gpio_init(void)
+{
+	//	NRF IRQ SETTINGS
+	//	MAKE DIRECTION INPUT PC4,PC3
+	PC_DDR &= ~( (1 << 3) | (1 << 4) ); // 3 FOR NRF1 --- 4 FOR NRF2
+	// For double device 
+	PA_DDR |= (1 << 3); // NRF1 CE
+	PA_DDR |= (1 << 2); // NRF2 CE
+	PA_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1 CE
+	PA_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2 CE
+	PA_ODR &= ~(1 << 3); // OUTPUT LOW NRF1 CE
+	PA_ODR &= ~(1 << 2); // OUTPUT LOW NRF2 CE
+	
+	//	NRF CSN SETTINGS SPI CHIP SELECT ACTIVE LOW
+	PD_DDR |= (1 << 3); // NRF1 CSN
+	PD_DDR |= (1 << 2); // NRF2 CSN
+	PD_CR1 |= (1 << 3); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF1 CSN
+	PD_CR1 |= (1 << 2); // PUSH-PULL,DEFAULT OPEN DRAIN // NRF2 CSN
+	PD_ODR |= (1 << 3); // OUTPUT HIGH NRF1 CSN
+	PD_ODR |= (1 << 2); // OUTPUT HIGH NRF2 CSN
+}
+
+void wait_irq(unsigned char which_nrf)
+{
+	// NRF1 IRQ PIN IS PC3 ---- READ PC3 PIN UNTIL BE LOW
+	if(which_nrf == 3)
+	{
+		while( !(PC_IDR & (1 << 3) ) );
+	}
+	else
+	{
+		while( !(PC_IDR & (1 << 4) ) );
+	}
+}
+
 void nrf24l01_init(unsigned char which_nrf)
 {
 	delay_ms(100);							// wait until it happens to power down mode 100ms
