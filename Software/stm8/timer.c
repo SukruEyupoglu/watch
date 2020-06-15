e770112ee15e54e89e59df3546ba39ba2d88f906
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-06-15 21:30:19 +0300

    Update timer.c

--- Software/stm8/timer.c
+++ Software/stm8/timer.c
@@ -1,4 +1,8 @@
 void timer4_init()
 {
-
+  // Frequency = F_CLK / (2 * prescaler * (1 + ARR))
+  // 160Hz = 16.000.000 / (2 * 200 * (1 + 249)
+  TIM4_PSCR = 200;
+  TIM4_ARR = 249;
+  TIM4_CR1 |= (1 << TIM4_CR1_CEN); // Enable TIM4
 }
