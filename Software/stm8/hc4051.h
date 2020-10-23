f9eb70cecfd277dbc556972c56a1db84906ac733
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2020-10-23 23:02:47 +0300

    Update hc4051.h

--- Software/stm8/hc4051.h
+++ Software/stm8/hc4051.h
@@ -1,1 +1,19 @@
-void select_out(void);
+
+
+#define MOTOR_1 0
+#define MOTOR_2 1
+#define MOTOR_3 2
+#define MOTOR_4 3
+#define OUT_SCK 4
+#define FLASH_SCK 5
+#define IN_SCK 6
+#define RF_SCK 7
+
+
+void select_out(unsigned char channel);
+
+
+
+
+
+
