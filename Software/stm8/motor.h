e8508c58493dffb426fcb2633eb9cfcda71a592d
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2021-01-09 14:52:52 +0300

    Update motor.h

--- Software/stm8/motor.h
+++ Software/stm8/motor.h
@@ -13,8 +13,8 @@ negative 10101010
 1 channels to close P-CHANNEL mosfets
 0 channels to close N-CHANNEL mosfets
 
-1010 1010 standby
-0011 1010 first step with only one coil
+0101 0101 standby
+0011 0101 first step with only one coil
 
 1010 0011 second weak full step release other coil
 
@@ -30,17 +30,17 @@ start_timer for hold motor without heating
 if start moving stop timer
 if stop moving start timer for hold motor without heating
 
-#define standby 1010_1010                                           0xAA  SB
-#define first_coil_forward_on                       0011_1010       0x3A  FCF
-#define second_coil_forward_on                      1010_0011       0xA3  SCF
-#define two_coil_forward_on                         0011_0011       0x33  TCF
-#define first_coil_backward_on                      1100_1010       0xCA  FCB
-#define second_coil_backward_on                     1010_1100       0xAC  SCB
-#define two_coil_backward_on                        1100_1100       0xCC  TCB
+#define standby 0101_0101                                           0x55  SB
+#define first_coil_forward_on                       0101_1100       0x5C  FCF
+#define second_coil_forward_on                      1100_0101       0xC5  SCF
+#define two_coil_forward_on                         1100_1100       0xCC  TCF
+#define first_coil_backward_on                      0101_0011       0x53  FCB
+#define second_coil_backward_on                     0011_0101       0x35  SCB
+#define two_coil_backward_on                        0011_0011       0x33  TCB
 #define first_coil_forward_second_coil_backward     0011_1100       0x3C  FCFSCB
-#define second_coil_forward_first_coil_backward     1100_0011       0x3C  SCFFCB
+#define second_coil_forward_first_coil_backward     1100_0011       0xC3  SCFFCB
 
-A = idle
+5 = idle
 3 = forward
 C = backward
 
