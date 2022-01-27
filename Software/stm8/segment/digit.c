fd21fac4158afa27568b3ea4b49968c58bdb61a3
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-01-27 10:57:59 +0300

    Update digit.c

--- Software/stm8/segment/digit.c
+++ Software/stm8/segment/digit.c
@@ -8,34 +8,7 @@
 unsigned char num_to_dig(unsigned char num);
 void idle_mode(void);
 
-int main(void)
-{
-	// CLK_CKDIVR = 0; // 16mhz
-	// default 2mhz
-	i2c_init();
-	spi_init();
-	
-	unsigned char d[0x13];
 
-	while(1)
-	{
-		// I2C TAKE THE CURSOR TO START
-		i2c_start();
-		i2c_write_addr(0xD0); // write
-		i2c_write(0x0); // cursor setting
-		i2c_stop();
-		// GET ALL DATA FROM DS3231 TO d[0x13]
-		i2c_start();
-		i2c_write_addr(0xD1); // read
-		i2c_read_arr(d,0x13);
-		i2c_stop();
-		// WRITE HOUR AND MINUTE
-		spi(num_to_dig(d[hour])); //first hour
-		spi(num_to_dig(d[minute])); //second minute
-		// WHILE IDLE MODE CHECK BOOT BUTTON EVERYTIME
-		idle_mode();  //  1 minute wait when check mode button
-	}
-}
 
 void idle_mode(void)
 {
