f1019e186bbe8e4942f27ee118d69e2306b5d184
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-01-28 09:20:10 +0300

    Update main.c

--- Software/stm8/segment/main.c
+++ Software/stm8/segment/main.c
@@ -5,6 +5,11 @@
 #include "beep.h"
 #include "gpio.h"
 
+void timer_isr() __interrupt(TIM1_ISR);
+void tim1_init(unsigned short sec);
+
+unsigned char tim1_interrupt_flag = 1;
+
 
 int main(void)
 {
@@ -40,3 +45,28 @@ int main(void)
 		idle_mode();  //  1 minute wait when check mode button
 	}
 }
+
+void timer_isr() __interrupt(TIM1_ISR) {
+  tim1_interrupt_flag = 1;
+  TIM1_SR &= ~(1 << TIM1_SR_UIF);
+}
+
+void tim1_init(unsigned short sec)
+{
+  // THIS TIMER MAKED FOR LONG TIMING , ONLY SECOND
+  // max second = 1638sec , min second = 1sec
+  // frequency = F_CLK / ( ( (TIM1_PSCRH << 8) + TIM1_PSCRL) * (1 + ( (TIM1_ARRH << 8) + TIM1_ARRL) ) )
+  TIM1_CR1    = 0;            // Disable TIM1
+  TIM1->PSCRH = ( ( sec * 40 ) >> 8);
+  TIM1->PSCRL = ( ( sec * 40 ) & 0xFF);
+  TIM1->ARRH = 0xC3;
+  TIM1->ARRL = 0x50;
+  
+  TIM1_CR1      |= (1 << TIM1_CR1_OPM); // stop at max value from ARR
+  TIM1_IER      |= (1 << TIM1_IER_UIE); // Enable Update Interrupt
+  TIM1->CR1 = TIM1_CR1_CEN; // Enable the counter
+}
+
+
+
+
