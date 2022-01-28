d521d0da1774c0d472a5ddf78e0222d31931b755
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-01-28 09:29:27 +0300

    Update main.c

--- Software/stm8/segment/main.c
+++ Software/stm8/segment/main.c
@@ -3,7 +3,7 @@
 #include "i2c.h"
 #include "spi.h"
 #include "beep.h"
-#include "gpio.h"
+#include "button.h"
 
 void timer_isr() __interrupt(TIM1_ISR);
 void tim1_init(unsigned short sec);
@@ -17,32 +17,33 @@ int main(void)
 	// CLK_CKDIVR = 0; // 16mhz
 	// default 2mhz
 	
-	// if u want close clocks for lower energy CLK_CKDIVR
-	
 	i2c_init();
 	spi_init();
-	
+	tim1_init(1);
 
 
 	while(1)
 	{
-		// I2C TAKE THE CURSOR TO START
-		i2c_start();
-		i2c_write_addr(0xD0); // write
-		i2c_write(0x0); // cursor setting
-		i2c_stop();
-		// GET ALL DATA FROM DS3231 TO d[0x13]
-		i2c_start();
-		i2c_write_addr(0xD1); // read
-		i2c_read_arr(d,0x13);
-		i2c_stop();
-		// WRITE HOUR AND MINUTE
-		spi(num_to_dig(d[hour])); //first hour
-		spi(num_to_dig(d[minute])); //second minute
-		// CHECK ALERT FLAG
-		check_alert();
-		// WHILE IDLE MODE CHECK BOOT BUTTON EVERYTIME
-		idle_mode();  //  1 minute wait when check mode button
+		if(tim1_interrupt_flag == 1)
+		{
+			// I2C TAKE THE CURSOR TO START
+			i2c_start();
+			i2c_write_addr(0xD0); // write
+			i2c_write(0x0); // cursor setting
+			i2c_stop();
+			// GET ALL DATA FROM DS3231 TO d[0x13]
+			i2c_start();
+			i2c_write_addr(0xD1); // read
+			i2c_read_arr(d,0x13);
+			i2c_stop();
+			// WRITE HOUR AND MINUTE
+			spi(num_to_dig(d[hour])); //first hour
+			spi(num_to_dig(d[minute])); //second minute
+			// CHECK ALERT FLAG
+			check_alert();
+			tim1_interrupt_flag = 0;
+		}
+		check_boot_button();
 	}
 }
 
