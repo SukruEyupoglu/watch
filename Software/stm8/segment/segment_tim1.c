cacb78db2951764ee557a5bfba727d8cdfee56be
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-01-28 09:13:16 +0300

    Update segment_tim1.c

--- Software/stm8/segment/segment_tim1.c
+++ Software/stm8/segment/segment_tim1.c
@@ -1,3 +1,6 @@
+
+unsigned char tim1_interrupt_flag = 0;
+
 void tim1_init(unsigned short sec)
 {
   // THIS TIMER MAKED FOR LONG TIMING , ONLY SECOND
@@ -15,35 +18,9 @@ void tim1_init(unsigned short sec)
 }
 
 void timer_isr() __interrupt(TIM1_ISR) {
-  
-  
-		// I2C TAKE THE CURSOR TO START
-		i2c_start();
-		i2c_write_addr(0xD0); // write
-		i2c_write(0x0); // cursor setting
-		i2c_stop();
-		// GET ALL DATA FROM DS3231 TO d[0x13]
-		i2c_start();
-		i2c_write_addr(0xD1); // read
-		i2c_read_arr(d,0x13);
-		i2c_stop();
-		// WRITE HOUR AND MINUTE
-		spi(num_to_dig(d[hour])); //first hour
-		spi(num_to_dig(d[minute])); //second minute
-		// CHECK ALERT FLAG
-		check_alert();
-  
-  
-    TIM1_SR &= ~(1 << TIM1_SR_UIF);
+  tim1_interrupt_flag = 1;
+  TIM1_SR &= ~(1 << TIM1_SR_UIF);
 }
-
-
-
-  /*
-  const uint16_t tim1_compare_reg1 = 4000; // 25% duty cycle
-  TIM1->CCR1H = (tim1_compare_reg1 >> 8);
-  TIM1->CCR1L = (tim1_compare_reg1 & 0xFF);
-  */
 
 
 
