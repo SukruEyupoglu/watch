902e30af3df67167895064694e7deacba92ecfa3
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-01-28 12:08:25 +0300

    Update main.c

--- Software/stm8/segment/main.c
+++ Software/stm8/segment/main.c
@@ -17,6 +17,11 @@ void place_ds3231_cursor(unsigned char x);
 #define DS3231_ALARM_MINUTE_ADDR 0x08
 #define DS3231_ALARM_HOUR_ADDR 0x09
 
+#define BOOT_BUTTON_PRESS ( !( PA_IDR & (1 << 3) ) )
+#define UP_BUTTON_PRESS ( !( PC_IDR & (1 << 3) ) )
+#define DOWN_BUTTON_PRESS ( !( PD_IDR & (1 << 3) ) )
+
+#define BUTTON_PRESSED 0
 
 // active high
 #define LATCH PC_ODR |= (1 << 4);PC_ODR &= ~(1 << 4)
@@ -111,6 +116,7 @@ void write_ds3231_minute(unsigned char minute)
 	    i2c_stop();
     }
 }
+			    
 void write_ds3231_hour(unsigned char hour)
 {
     if(hour < 12)
@@ -132,9 +138,171 @@ void place_ds3231_cursor(unsigned char x)
 			i2c_stop();
 }
 
+void button_init(void)
+{
+  //  MAKE PA3 BOOT BUTTON INPUT
+  PA_DDR &= ~(1 << 3);
+  //  MAKE PC3 UP BUTTON INPUT
+  PC_DDR &= ~(1 << 3);  
+  //  MAKE PD3 DOWN BUTTON INPUT
+  PD_DDR &= ~(1 << 3);  
+}
 
+void check_boot_button(void)
+{
+  if(BOOT_BUTTON_PRESS)
+  {
+    while(BOOT_BUTTON_PRESS);
+    turn_on_minute_dot_light();
+    boot_button_first_pressed_function();
+  }
+}
 
+void boot_button_first_pressed_function(void)
+{
+  while(1)
+  {
+    if(BOOT_BUTTON_PRESS)
+    {
+      while(BOOT_BUTTON_PRESS);
+      turn_on_hour_dot_light();
+      boot_button_second_pressed_function();
+      break;
+    }
+    if(UP_BUTTON_PRESS)
+    {
+      increase_minute();
+      while(UP_BUTTON_PRESS);
+    }
+    if(DOWN_BUTTON_PRESS)
+    {
+      decrease_minute();
+      while(DOWN_BUTTON_PRESS);
+    }
+  }
+}
+			    
+void boot_button_second_pressed_function(void)
+{
+  while(1)
+  {
+    if(BOOT_BUTTON_PRESS)
+    {
+      while(BOOT_BUTTON_PRESS);
+      boot_button_third_pressed_function();
+      break;
+    }
+    if(UP_BUTTON_PRESS)
+    {
+      increase_hour();
+      while(UP_BUTTON_PRESS);
+    }
+    if(DOWN_BUTTON_PRESS)
+    {
+      decrease_hour();
+      while(DOWN_BUTTON_PRESS);
+    }
+  }
+}
+			    
+void boot_button_third_pressed_function(void)
+{
+  save_changes();
+}
 
+unsigned char num2dig(unsigned char num)
+{
+	switch(num)
+	{
+		case 0:
+		{		     // .GFE DCBA 7 segment display
+			return 0x3F; // 0011 1111 
+		}
+		break;
+		case 1:
+		{
+			return 0x06; // 0000 0110 
+		}
+		break;
+		case 2:
+		{
+			return 0x5B; // 0101 1011 
+		}
+		break;
+		case 3:
+		{
+			return 0x4F; // 0100 1111 
+		}
+		break;
+		case 4:
+		{
+			return 0x66; // 0110 0110 
+		}
+		break;
+		case 5:
+		{
+			return 0x6C; // 0110 1101 
+		}
+		break;
+		case 6:
+		{
+			return 0x7C; // 0111 1101 
+		}
+		break;
+		case 7:
+		{
+			return 0x07; // 0000 0111 
+		}
+		break;
+		case 8:
+		{
+			return 0x7F; // 0111 1111 
+		}
+		break;
+		case 9:
+		{
+			return 0x6F; // 0110 1111 
+		}
+		break;
+		case 10:
+		{
+			return 0x70; // 0111 0000 
+		}
+		break;
+		case 11:
+		{
+			return 0x36; // 0011 0110 
+		}
+		break;
+		default:
+		{
+			return 0x0;
+		}	
+	}
+	return 0x0;
+}
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
+			    
 
 
 
