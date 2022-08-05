c0210feca87c842728758fe39dc28a853413e62e
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-05 12:06:17 +0300

    Create spi.h

---
+++ Software/stm8/Char_LCD/hc595/spi/spi.h
@@ -0,0 +1,21 @@
+// if using uint8_t or uint16_t or uint32_t
+// this must include
+// #include <stdint.h>
+
+/*
+ * Initialize SPI in MODE1.
+ *
+ * Pinout:
+ * SCK  -> PC5
+ * MOSI -> PC6
+ * MISO -> PC7
+ * CS   -> user defined
+ */
+ 
+
+void spi_init(void);
+unsigned char spi(unsigned char data);
+
+
+
+
