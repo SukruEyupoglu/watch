4c8a5a98e8efde29beb36cbd45961902c30dabd4
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-05 16:46:44 +0300

    Update char_lcd.c

--- Software/stm8/char_lcd/char_lcd.c
+++ Software/stm8/char_lcd/char_lcd.c
@@ -12,9 +12,9 @@ void char_lcd_4_bit_init(void)
 	
 }
 
-void char_lcd_4_bit_send(unsigned char data[20])
+void char_lcd_4_bit_send(unsigned char data)
 {
-	
+	// hc595_send( 0x0F & (data >> 4) );
 }
 
 
