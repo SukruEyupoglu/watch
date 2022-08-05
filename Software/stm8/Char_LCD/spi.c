c03f9936466d8c9f54b5245f5aab6d28cf771a64
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-05 11:03:25 +0300

    Update spi.c

--- Software/stm8/Char_LCD/spi.c
+++ Software/stm8/Char_LCD/spi.c
@@ -24,19 +24,5 @@ unsigned char spi(unsigned char data)
   while ( (SPI_SR & (1 << SPI_SR_RXNE) ) != (1 << SPI_SR_RXNE) );
   return SPI_DR;
 }
-void hc595_init(void)
-{
-  spi_init();
-  PC_DDR |= (1 << 7); // SPI MISO PIN SET AS GPIO OUTPUT FOR LATCH
-  PC_CR1 |= (1 << 7); // MAKE PUSH-PULL,DEFAULT MAYBE OPEN DRAIN FOR LATCH
-  PC_ODR &= ~(1 << 7); // LATCH OUTPUT LOW
-}
-void hc595_latch(void)
-{
-  // ACTIVE HIGH FOR 74HC595 LATCH(STCP) PIN LOW-TO-HIGH FOR ENABLE
-  PC_ODR |= (1 << 7);
-  PC_ODR &= ~(1 << 7);
-}
-
 
 
