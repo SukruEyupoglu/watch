209fc18b1f58c601bf3d0c533fd242d5cea425ae
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-05 12:05:10 +0300

    Create spi.c

---
+++ Software/stm8/Char_LCD/hc595/spi/spi.c
@@ -0,0 +1,30 @@
+#include "stm8s.h"
+#include "spi.h"
+
+void spi_init(void)
+{
+  // 500KHZ spi clk
+  // SPI_CR1_MSTR  = spi master bit
+  // SPI_CR1_SPE = spi enable bit
+  // SPI_CR1_BR = baud rate bits[3,4,5] divider 
+  // SPI_CR1_BR = 000; --> FMASTER / 2 --> 16 / 2 = 8 mhz
+  // SPI_CR1_BR = 100; --> FMASTER / 32 --> 16 / 32 = 500 khz
+  // SPI_CR1 = (1 << SPI_CR1_MSTR) | (1 << SPI_CR1_SPE) | (1 << SPI_CR1_BR1);
+  
+  // 8MHZ spi clk
+  SPI_CR1 = (1 << SPI_CR1_MSTR) | (1 << SPI_CR1_SPE);
+  // NOT CHANGE THIS READ DATASHEET
+  //SPI_CR2 = (1 << SPI_CR2_SSM) | (1 << SPI_CR2_SSI) | (1 << SPI_CR2_BDM) | (1 << SPI_CR2_BDOE);
+}
+
+unsigned char spi(unsigned char data)
+{
+  while ( (SPI_SR & (1 << SPI_SR_TXE) ) != (1 << SPI_SR_TXE) );
+  SPI_DR = data;
+  while ( (SPI_SR & (1 << SPI_SR_RXNE) ) != (1 << SPI_SR_RXNE) );
+  return SPI_DR;
+}
+
+
+
+
