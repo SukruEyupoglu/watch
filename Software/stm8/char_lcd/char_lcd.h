f5ca591b23d434f7592eb1f43ef37c8eaf78fb1b
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-05 16:50:15 +0300

    Update char_lcd.h

--- Software/stm8/char_lcd/char_lcd.h
+++ Software/stm8/char_lcd/char_lcd.h
@@ -1,4 +1,10 @@
+
+#define LCD_EN (1 << 4)
+#define LCD_RW (1 << 5)
+#define LCD_RS (1 << 6)
+
+
 void lcd_init_4_bit(void);
-void lcd_send_4_bit(unsigned char data[20]);
+void lcd_send_4_bit(unsigned char data);
 
 
