29dc4b8c3fe85fe9cc7423ebb7d4c6013441dc44
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-08 09:30:43 +0300

    Update char_lcd.c

--- Software/stm8/char_lcd/char_lcd.c
+++ Software/stm8/char_lcd/char_lcd.c
@@ -9,14 +9,39 @@
 
 void char_lcd_4_bit_init(void)
 {
+	unsigned char cmd = 0;
+	cmd |= LCD_EN;
+	cmd &= ~LCD_RW;
+	cmd &= ~LCD_RS;
+	delay_ms(15); // power-on initialization time
+	hc595_send(cmd);
+	delay_ms(3);
+	char_lcd_4_bit_send( cmd , 0x02);
+	char_lcd_4_bit_send( cmd , 0x20);
+	char_lcd_4_bit_send( cmd , 0x01);
+	char_lcd_4_bit_send( cmd , 0x0C);
+	char_lcd_4_bit_send( cmd , 0x06);	
+}
+
+void char_lcd_4_bit_send(unsigned char cmd , unsigned char data)
+{
+	// SEND UPPER 4 BIT FIRST
+	hc595_send( cmd | (data >> 4) );
+	hc595_send( ( cmd | (data >> 4) ) & ( ~LCD_EN ) );
+	delay_ms(1);
+	hc595_send( cmd | (data >> 4) );
+	delay_ms(3);
+	// SEND LOWER 4 BIT SECOND
+	hc595_send( cmd | (0x0F & data) );
+	hc595_send( ( cmd | (0x0F & data) ) & ( ~LCD_EN ) );
+	delay_ms(1);
+	hc595_send( cmd | (0x0F & data) );
+	delay_ms(3);
+}
+
+void char_lcd_4_bit_write(unsigned char data)
+{
 	
 }
-
-void char_lcd_4_bit_send(unsigned char data)
-{
-	// hc595_send( 0x0F & (data >> 4) );
-}
-
-
 
 
