31a1b77eca13ea776473ab97ca4c52416425eb70
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-08 11:11:09 +0300

    Update stm8flash.sh

--- Software/stm8/sh/stm8flash.sh
+++ Software/stm8/sh/stm8flash.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 ./stm8flash -c stlinkv2 -p stm8s103f3 -s flash -w stm8/stm8.ihx
-
-
+# ./stm8flash -c stlinkv2 -p stm8s003f3 -s flash -w stm8/stm8.ihx
+# ./stm8flash -c stlinkv2 -p stm8s003f3 -s eeprom -w eepromdata_128_byte.bin
+# ./stm8flash -c stlinkv2 -p stm8s003f3 -s eeprom -r eepromdata_128_byte.bin
+# ./stm8flash -c stlinkv2 -p stm8s003f3 -s eeprom -v eepromdata_128_byte.bin
