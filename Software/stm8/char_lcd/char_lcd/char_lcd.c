cd589818d822de61ac3e45298a8b93a8d4499ed8
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-25 16:47:01 +0300

    Update char_lcd.c

--- Software/stm8/char_lcd/char_lcd/char_lcd.c
+++ Software/stm8/char_lcd/char_lcd/char_lcd.c
@@ -24,51 +24,35 @@ void char_lcd_cmd(unsigned char cmd)
     delay_ms(10);
 }
 
+void char_lcd_data(unsigned char data)
+{
+    hc595_send( ( (data >> 4) & 0x0F) | (LCD_EN | LCD_RS) ); // send high 4 bit with EN-HIGH
+    delay_ms(1);
+    hc595_send( ( (data >> 4) & 0x0F) | LCD_RS); // ONLY EN HIGH-TO-LOW FOR ENABLE
+    delay_ms(10);
+
+    hc595_send( (data & 0x0F) | (LCD_EN | LCD_RS) ); // send high 4 bit with EN-HIGH
+    delay_ms(1);
+    hc595_send( (data & 0x0F) | LCD_RS); // ONLY EN HIGH-TO-LOW FOR ENABLE
+    delay_ms(10);
+}
 
 void char_lcd_4_bit_init(void)
 {
 	delay_ms(15); // power-on initialization time
-	char_lcd_4_bit_send_command(0x00);
-	char_lcd_4_bit_send_command(0x02);
-	char_lcd_4_bit_send_command(0x20);
-	char_lcd_4_bit_send_command(0x01);
-	char_lcd_4_bit_send_command(0x0C);
-	char_lcd_4_bit_send_command(0x06);	
+	char_lcd_cmd(0x02);
+	char_lcd_cmd(0x28);
+	char_lcd_cmd(0x0E);
 }
 
-void char_lcd_4_bit_send(unsigned char cmd , unsigned char data)
-{
-	// SEND UPPER 4 BIT FIRST
-	hc595_send( cmd | (data >> 4) );
-	hc595_send( ( cmd | (data >> 4) ) & ( ~LCD_EN ) );
-	delay_ms(1);
-	hc595_send( cmd | (data >> 4) );
-	delay_ms(3);
-	// SEND LOWER 4 BIT SECOND
-	hc595_send( cmd | (0x0F & data) );
-	hc595_send( ( cmd | (0x0F & data) ) & ( ~LCD_EN ) );
-	delay_ms(1);
-	hc595_send( cmd | (0x0F & data) );
-	delay_ms(3);
-}
-
-void char_lcd_4_bit_send_data(unsigned char data)
-{
-	char_lcd_4_bit_send( ( LCD_EN | LCD_RS ) , data);
-}
-void char_lcd_4_bit_send_command(unsigned char cmd)
-{
-	char_lcd_4_bit_send( LCD_EN , cmd);
-}
-			    
 void char_lcd(unsigned char data[20] , unsigned char size)
 {
 	unsigned char f;
-	char_lcd_4_bit_send_command(0x01); // clear lcd
-	char_lcd_4_bit_send_command(0x80); // set coursor to 0
+	char_lcd_cmd(0x01); // clear lcd
+	char_lcd_cmd(0x80); // set coursor to 0
 	if( size > 20 ) return;
 	for( f = 0 ; f < size ; f++)
 	{
-		char_lcd_4_bit_send_data(data[f]);
+		char_lcd_data(data[f]);
 	}
 }
