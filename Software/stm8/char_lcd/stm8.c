8f532302c6fd14655be963f862da736bcae491da
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-26 14:16:28 +0300

    Update stm8.c

--- Software/stm8/char_lcd/stm8.c
+++ Software/stm8/char_lcd/stm8.c
@@ -2,15 +2,16 @@
 
 int main()
 {
+	// code example for array
 	unsigned char data[20] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J'};
 	unsigned char size = 5;
 	
 	char_lcd_4_bit_init();
-	char_lcd(*data , size);
+	char_lcd(data , size); //code example for array 
 	
 	while(1)
 	{
-	char_lcd(*data , size);
+	//char_lcd(data , size);
 	}
 return 0;
 }
