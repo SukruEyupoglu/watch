b3d0415fcceea6af8d20d68d62ddcb303b644321
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-08-31 09:17:48 +0300

    Update adc.c

--- Software/stm8/adc.c
+++ Software/stm8/adc.c
@@ -20,3 +20,41 @@ unsigned char adc_read(unsigned char channel,unsigned char clk_div)
 	// return only MSB
 	return ADC1_DRH;;
 }
+
+// ADC1 MAX CONVERSION SPEED , ONLY 3,4,5,6 CHANNEL FOR STM8S003C8T6
+void adc(unsigned char channel , unsigned char count , int *data_h)
+{
+	unsigned char f;
+	// close ADC
+	ADC1_CR1 &= ~(1 << ADC1_CR1_ADON);
+	// clear EOC flag
+	ADC1_CSR &= ~(1 << ADC1_CSR_EOC);
+	// write channel
+	ADC1_CSR |= channel;
+	// set CONT bit for continuous conversion
+	ADC1_CR1 |= (1 << ADC1_CR1_CONT);
+	
+	// power up ADC
+	ADC1_CR1 |= (1 << ADC1_CR1_ADON);
+	// start conversion
+	ADC1_CR1 |= (1 << ADC1_CR1_ADON);
+	
+	for(f = 0 ; f < count ; f++)
+	{
+		// wait end of conversion
+		while (!(ADC1_CSR & (1 << ADC1_CSR_EOC)));
+		// add sum of conversion
+		*data_h += ADC1_DRH / count;
+		// clear EOC flag for start new converion
+		ADC1_CSR &= ~(1 << ADC1_CSR_EOC);
+	}
+	// close ADC
+	ADC1_CR1 &= ~(1 << ADC1_CR1_ADON);
+}
+
+
+
+
+
+
+
