9527b1b5031a309a855f45d6f37b2f1f0da4d244
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-09-02 08:20:34 +0300

    Update adc.h

--- Software/stm8/measure/adc/adc.h
+++ Software/stm8/measure/adc/adc.h
@@ -3,3 +3,12 @@
 //  clk_div 4 or 8 or 12 Fmaster/clk_div
 unsigned char adc_read(unsigned char channel,unsigned char clk_div);
 
+void adc(unsigned char channel , unsigned char count , int *data_h);
+  
+  
+  
+  
+  
+  
+  
+  
