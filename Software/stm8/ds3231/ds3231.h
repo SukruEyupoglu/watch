5391665599c6f77d3aad56bade39f9d412b24929
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-11-28 11:36:34 +0300

    Create ds3231.h

---
+++ Software/stm8/ds3231/ds3231.h
@@ -0,0 +1,19 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DS3231_MINUTE_ADDR 0x01
+#define DS3231_HOUR_ADDR 0x02
+#define DS3231_STATUS_ADDR 0x0F
+
+#define DS3231_ALARM_MINUTE_ADDR 0x08
+#define DS3231_ALARM_HOUR_ADDR 0x09
+  
+
+#ifdef __cplusplus
+}
+#endif
+  
+  
+  
+  
