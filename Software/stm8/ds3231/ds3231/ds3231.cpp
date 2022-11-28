f911e6baea0ba9b9f17d1467026eac8211761c79
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-11-28 14:46:09 +0300

    Update ds3231.cpp

--- Software/stm8/ds3231/ds3231/ds3231.cpp
+++ Software/stm8/ds3231/ds3231/ds3231.cpp
@@ -1,11 +1,11 @@
-restart_counting()
+void restart_counting(void)
 {
   // RESET FLAGS FOR CONTINUE
   i2c_start();
   i2c_write_addr(0xD0);
   i2c_write(0x0E);
-  i2c_write(0x00); 
-  i2c_write(0x00); //i2c_write(0x0F);
+  i2c_write(0x7F & data[0x0E]);  // control register
+  i2c_write(0x7F & data[0x0F]);  // control/status register
   i2c_stop();
 }
 
