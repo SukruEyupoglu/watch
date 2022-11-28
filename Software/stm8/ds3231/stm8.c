d77d522ced7ad0ac6005a6e1abe7991526641141
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-11-28 11:43:20 +0300

    Create stm8.c

---
+++ Software/stm8/ds3231/stm8.c
@@ -0,0 +1,17 @@
+#include "char_lcd/char_lcd.h"
+
+int main()
+{
+	// code example for array
+	unsigned char data[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	unsigned char size = 5;
+	
+	char_lcd_4_bit_init();
+	char_lcd(data , size); //code example for array 
+	
+	while(1)
+	{
+	//char_lcd(data , size);
+	}
+return 0;
+}
