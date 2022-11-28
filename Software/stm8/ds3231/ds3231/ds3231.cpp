674b943727f342ea11e84a341dd5a66e817355d2
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-11-28 14:57:03 +0300

    Update ds3231.cpp

--- Software/stm8/ds3231/ds3231/ds3231.cpp
+++ Software/stm8/ds3231/ds3231/ds3231.cpp
@@ -1,7 +1,12 @@
 #include "../stm8s.h"
 #include "i2c/i2c.h"
 
-
+void ds3231::read_data(void)
+{
+  i2c_start();
+  i2c_write_addr(0xD1); // read
+  i2c_read_arr(data,0x13); // inside i2c_stop(); there are
+}
 
 void ds3231::restart_counting(void)
 {
@@ -16,11 +21,11 @@ void ds3231::restart_counting(void)
 
 void ds3231::place_cursor(unsigned char x)
 {
-// I2C TAKE THE CURSOR TO START
-i2c_start();
-i2c_write_addr(0xD0); // write
-i2c_write(x); // cursor setting
-i2c_stop();
+  // I2C TAKE THE CURSOR TO START
+  i2c_start();
+  i2c_write_addr(0xD0); // write
+  i2c_write(x); // cursor setting
+  i2c_stop();
 }
 
 
