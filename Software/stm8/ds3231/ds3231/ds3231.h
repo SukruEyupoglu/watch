92640bf5471a6d1b73db9fe48eb8481ca3925001
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2022-11-28 15:50:16 +0300

    Update ds3231.h

--- Software/stm8/ds3231/ds3231/ds3231.h
+++ Software/stm8/ds3231/ds3231/ds3231.h
@@ -12,10 +12,11 @@ extern "C" {
 class ds3231 {
 	public:
 		unsigned char data[0x13];
-		void read_data(void);
+		void read_data(void); // read all data from ds3231 and convert with reg2time sec-min-hour
 		void place_cursor(unsigned char x);
 		void restart_counting(void);
-    
+		unsigned char time2reg(unsigned char time);
+		unsigned char reg2time(unsigned char reg);
 	private:
   
 	protected:
