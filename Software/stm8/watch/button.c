e37ab41902c838ed7f77dba94a487ff103a3f0f8
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2023-07-27 13:38:49 +0300

    Create button.c

---
+++ Software/stm8/watch/button.c
@@ -0,0 +1,23 @@
+// 3 button
+// no long press
+// only one press
+// mode - up - down
+// workflow process
+/*
+1 - minute
+2 - hour
+3 - day of week
+4 - date
+5 - month
+6 - year
+7 - first alarm minute
+8 - first alarm hour
+9 - second alarm minute
+10 - second alarm hour
+
+
+*/
+
+
+
+
