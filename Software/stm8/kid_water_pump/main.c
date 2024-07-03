0e6677d66a8c070386c1a7ef31e2bbed5d1f1ba6
ŞÜKRÜ EYÜPOĞLU <48905447+SukruEyupoglu@users.noreply.github.com>
2024-07-03 14:28:51 +0300

    Update main.c

--- Software/stm8/kid_water_pump/main.c
+++ Software/stm8/kid_water_pump/main.c
@@ -1,5 +1,7 @@
 void timer4_init(unsigned char us)
 {
+  // CLK_CKDIVR = 0x1F; // minimum F_CLK = 15625
+  // 4.17second = 15625 / (2 * 128 * 255) = maximum wait time
   // frequency = F_CLK / (2 * prescaler * (1 + ARR))
   //  125Hz = 16.000.000 / (2 * 128 * (1 + 249)
   //  10us = 16.000.000 / (2 * 8 * (1 + 9) (1 step per 10us)
